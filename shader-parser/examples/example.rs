c67d3a6a93d07248395ffb5c0d9243cbc2491788
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-01-31 13:18:58 +0100

    Use the glsl to spirv compiler in shader-parser

--- shader-parser/examples/example.rs
+++ shader-parser/examples/example.rs
@@ -1,9 +1,21 @@
+extern crate glsl_to_spirv;
 extern crate shader_parser;
 
-use std::io::Cursor;
-
 fn main() {
-    let content = include_bytes!("example.spv");
-    let output = shader_parser::reflect(Cursor::new(&content[..])).unwrap();
+    let shader = r#"
+#version 450
+
+uniform vec4 u_test;
+
+vec4 f_color;
+
+void main() {
+    f_color = u_test;
+}
+
+"#;
+
+    let content = glsl_to_spirv::compile(Some((shader, glsl_to_spirv::ShaderType::Fragment))).unwrap();
+    let output = shader_parser::reflect(content).unwrap();
     println!("{}", output);
 }
