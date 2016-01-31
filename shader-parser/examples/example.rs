be43ab8218aa91591465b03be3cb7decb900d3f5
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-01-31 14:29:53 +0100

    Slightly change example

--- shader-parser/examples/example.rs
+++ shader-parser/examples/example.rs
@@ -5,13 +5,23 @@ fn main() {
     let shader = r#"
 #version 450
 
+struct S {
+    vec3 val1;
+    bool val2[5];
+};
+
 uniform sampler2D u_texture;
+uniform S u_data;
 
 in vec2 v_texcoords;
 out vec4 f_color;
 
 void main() {
-    f_color = texture(u_texture, v_texcoords);
+    if (u_data.val2[4]) {
+        f_color = texture(u_texture, v_texcoords);
+    } else {
+        f_color = vec4(u_data.val1, 1.0);
+    }
 }
 
 "#;
