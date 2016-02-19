8d951c874bf515e833b40e661eb917dcb247b0fd
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-02-19 09:10:46 +0100

    Triangle example working on AMD

--- vulkano/examples/triangle_vs.glsl
+++ vulkano/examples/triangle_vs.glsl
@@ -3,7 +3,7 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
 
-in vec2 position;
+layout(location = 0) in vec2 position;
 
 void main() {
     gl_Position = vec4(position, 0.0, 1.0);
