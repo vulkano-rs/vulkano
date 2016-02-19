6e465a8c63c1b13c4ee9f99405f3979c11078878
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-02-19 16:44:35 +0100

    Make glslang work on Linux

--- glsl-to-spirv/build/build.rs
+++ glsl-to-spirv/build/build.rs
@@ -1,38 +1,25 @@
+extern crate cmake;
+
 use std::env;
 use std::fs;
-use std::fs::Permissions;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-changed=build/glslangValidator");
     println!("cargo:rerun-if-changed=build/glslangValidator.exe");
 
     let target = env::var("TARGET").unwrap();
     let out_file = Path::new(&env::var("OUT_DIR").unwrap()).join("glslang_validator");
 
     let path = if target.contains("windows") {
-        Path::new("build/glslangValidator.exe")
-    } else if target.contains("linux") {
-        Path::new("build/glslangValidator")
+        // TODO: check the hash of the file to make sure that it is not altered
+        Path::new("build/glslangValidator.exe").to_owned()
     } else {
-        panic!("The platform `{}` is not supported", target);
+        // TODO: automatically initialize the submodule if it wasn't done
+        cmake::build("glslang");
+        Path::new(&env::var("OUT_DIR").unwrap()).join("bin").join("glslangValidator")
     };
 
     if let Err(_) = fs::hard_link(&path, &out_file) {
         fs::copy(&path, &out_file).expect("failed to copy executable");
-    }
-
-    // TODO: check the hash of the files to make sure that they are not altered
-
-    // setting permissions of the executable
-    {
-        #[cfg(unix)] fn permissions() -> Option<Permissions> {
-            use std::os::unix::fs::PermissionsExt;
-            Some(Permissions::from_mode(755))
-        }
-        #[cfg(not(unix))] fn permissions() -> Option<Permissions> { None }
-        if let Some(permissions) = permissions() {
-            fs::set_permissions(&out_file, permissions).expect("failed to set permissions");
-        }
     }
 }
