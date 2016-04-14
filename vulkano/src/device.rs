f64bf72ab9cc7964cb82456f36007344f2fa7226
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-04-14 14:18:04 +0200

    Add failing test for having one Arc ref to a Device

--- vulkano/src/device.rs
+++ vulkano/src/device.rs
@@ -413,3 +413,15 @@ unsafe impl SynchronizedVulkanObject for Queue {
         self.queue.lock().unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    #[test]
+    #[ignore]   // FIXME: failing, fix me
+    fn one_ref() {
+        let (mut device, _) = gfx_dev_and_queue!();
+        assert!(Arc::get_mut(&mut device).is_some());
+    }
+}
