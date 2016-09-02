6f44ba6d717c8e1d66e7781cc81efcd887a39073
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-09-02 19:08:24 +0200

    Clean up image trait

--- vulkano/src/buffer/traits.rs
+++ vulkano/src/buffer/traits.rs
@@ -8,11 +8,9 @@
 // according to those terms.
 
 use std::any::Any;
-use std::ops::Range;
 use std::sync::Arc;
 
 use buffer::sys::UnsafeBuffer;
-use command_buffer::Submission;
 use device::Queue;
 use image::Image;
 use memory::Content;
@@ -24,42 +22,10 @@ use sync::Semaphore;
 
 use VulkanObject;
 
+// TODO: remove 'static + Send + Sync
 pub unsafe trait Buffer: 'static + Send + Sync {
     /// Returns the inner buffer.
     fn inner(&self) -> &UnsafeBuffer;
-
-    /// Returns whether accessing a range of this buffer should signal a fence.
-    fn needs_fence(&self, write: bool, Range<usize>) -> Option<bool>;
-
-    /// Called when a command buffer that uses this buffer is being built.
-    ///
-    /// Must return true if the command buffer should include a pipeline barrier at the start,
-    /// to read from what the host wrote, and a pipeline barrier at the end, to flush caches and
-    /// allows the host to read the data.
-    fn host_accesses(&self, block: usize) -> bool;
-
-    /// Given a range, returns the list of blocks which each range is contained in.
-    ///
-    /// Each block must have a unique number. Hint: it can simply be the offset of the start of the
-    /// block.
-    /// Calling this function multiple times with the same parameter must always return the same
-    /// value.
-    /// The return value must not be empty.
-    fn blocks(&self, range: Range<usize>) -> Vec<usize>;
-
-    /// Returns the range of bytes of the buffer slice used by a block.
-    fn block_memory_range(&self, block: usize) -> Range<usize>;
-
-    ///
-    ///
-    /// If the host is still accessing the buffer, this function implementation should block
-    /// until it is no longer the case.
-    ///
-    /// **Important**: The `Submission` object likely holds an `Arc` to `self`. Therefore you
-    ///                should store the `Submission` in the form of a `Weak<Submission>` and not
-    ///                of an `Arc<Submission>` to avoid cyclic references.
-    unsafe fn gpu_access(&self, ranges: &mut Iterator<Item = AccessRange>,
-                         submission: &Arc<Submission>) -> GpuAccessResult;
 
     #[inline]
     fn size(&self) -> usize {
@@ -185,17 +151,6 @@ unsafe impl<B> Buffer for Arc<B> where B: Buffer {
         (**self).inner()
     }
 
-    fn needs_fence(&self, _: bool, _: Range<usize>) -> Option<bool> { unimplemented!() }
-
-    fn host_accesses(&self, _: usize) -> bool { unimplemented!() }
-
-    fn blocks(&self, _: Range<usize>) -> Vec<usize> { unimplemented!() }
-
-    fn block_memory_range(&self, _: usize) -> Range<usize> { unimplemented!() }
-
-    unsafe fn gpu_access(&self, _: &mut Iterator<Item = AccessRange>,
-                         _: &Arc<Submission>) -> GpuAccessResult { unimplemented!() }
-
     #[inline]
     fn size(&self) -> usize {
         (**self).size()
@@ -229,16 +184,4 @@ pub unsafe trait TypedBuffer: Buffer {
     fn len(&self) -> usize where Self::Content: Content {
         self.size() / <Self::Content as Content>::indiv_size()
     }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct AccessRange {
-    pub block: usize,
-    pub write: bool,
-}
-
-pub struct GpuAccessResult {
-    pub dependencies: Vec<Arc<Submission>>,
-    pub additional_wait_semaphore: Option<Arc<Semaphore>>,
-    pub additional_signal_semaphore: Option<Arc<Semaphore>>,
 }
