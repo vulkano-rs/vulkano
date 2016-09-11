e0c3dfee5d0a6ed126b105a85a55c64d12c1dbb9
batanick <botanick333@gmail.com>
2016-09-11 14:11:45 +0200

    Teapot example: rotation added

--- examples/src/bin/teapot_vs.glsl
+++ examples/src/bin/teapot_vs.glsl
@@ -18,11 +18,13 @@ layout(location = 1) in vec3 normal;
 layout(location = 0) out vec3 v_normal;
 
 layout(set = 0, binding = 0) uniform Data {
-    mat4 worldview;
+    mat4 world;
+    mat4 view;
     mat4 proj;
 } uniforms;
 
 void main() {
-    v_normal = transpose(inverse(mat3(uniforms.worldview))) * normal;
-    gl_Position = uniforms.proj * uniforms.worldview * vec4(position, 1.0);
+    mat3 worldview = mat3(uniforms.view * uniforms.world);
+    v_normal = transpose(inverse(worldview)) * normal;
+    gl_Position = uniforms.proj * uniforms.view * uniforms.world * vec4(position, 1.0);
 }
