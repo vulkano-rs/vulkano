15550e683e855adeaeb418f1238fd20be261eb11
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-10-22 13:13:07 +0200

    Remove TODO in bind_pipeline

--- vulkano/src/command_buffer/cmd/bind_pipeline.rs
+++ vulkano/src/command_buffer/cmd/bind_pipeline.rs
@@ -85,8 +85,6 @@ impl<L, P> CmdBindPipeline<L, P> where L: CommandsList {
 
         assert_eq!(self.device.internal_object(), builder.device.internal_object());
 
-        // TODO: don't bind if not necessary
-
         // Returning now if the pipeline object is already bound.
         // Note that we need to perform this check after validating the device, otherwise the
         // pipeline ID could match by mistake.
@@ -108,6 +106,7 @@ impl<L, P> CmdBindPipeline<L, P> where L: CommandsList {
             _ => unreachable!()
         }
 
+        // Binding for real.
         unsafe {
             let vk = builder.device.pointers();
             let cmd = builder.command_buffer.clone().take().unwrap();
