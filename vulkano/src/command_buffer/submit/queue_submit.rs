2c3ff5ef12c94bda602c7f3a6e5d30784d795906
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-02-27 10:59:59 +0100

    Add documentation and tests for SubmitCommandBufferBuilder

--- vulkano/src/command_buffer/submit/queue_submit.rs
+++ vulkano/src/command_buffer/submit/queue_submit.rs
@@ -29,6 +29,7 @@ use VulkanPointers;
 use SynchronizedVulkanObject;
 
 /// Prototype for a submission that executes command buffers.
+// TODO: example here
 #[derive(Debug)]
 pub struct SubmitCommandBufferBuilder<'a> {
     wait_semaphores: SmallVec<[vk::Semaphore; 16]>,
@@ -54,6 +55,23 @@ impl<'a> SubmitCommandBufferBuilder<'a> {
     }
 
     /// Returns true if this builder will signal a fence when submitted.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use vulkano::command_buffer::submit::SubmitCommandBufferBuilder;
+    /// use vulkano::sync::Fence;
+    /// # let device: std::sync::Arc<vulkano::device::Device> = return;
+    ///
+    /// unsafe {
+    ///     let fence = Fence::new(device.clone()).unwrap();
+    ///
+    ///     let mut builder = SubmitCommandBufferBuilder::new();
+    ///     assert!(!builder.has_fence());
+    ///     builder.set_fence_signal(&fence);
+    ///     assert!(builder.has_fence());
+    /// }
+    /// ```
     #[inline]
     pub fn has_fence(&self) -> bool {
         self.fence != 0
@@ -61,19 +79,72 @@ impl<'a> SubmitCommandBufferBuilder<'a> {
 
     /// Adds an operation that signals a fence after this submission ends.
     ///
-    /// If a fence was previously set, it will no longer be signaled.
+    /// # Example
+    ///
+    /// ```
+    /// use std::time::Duration;
+    /// use vulkano::command_buffer::submit::SubmitCommandBufferBuilder;
+    /// use vulkano::sync::Fence;
+    /// # let device: std::sync::Arc<vulkano::device::Device> = return;
+    /// # let queue: std::sync::Arc<vulkano::device::Queue> = return;
+    ///
+    /// unsafe {
+    ///     let fence = Fence::new(device.clone()).unwrap();
+    ///
+    ///     let mut builder = SubmitCommandBufferBuilder::new();
+    ///     builder.set_fence_signal(&fence);
+    ///
+    ///     builder.submit(&queue).unwrap();
+    ///
+    ///     // We must not destroy the fence before it is signaled.
+    ///     fence.wait(Duration::from_secs(5)).unwrap();
+    /// }
+    /// ```
+    ///
+    /// # Safety
+    ///
+    /// - The fence must not be signaled at the time when you call `submit()`.
+    ///
+    /// - If you use the fence for multiple submissions, only one at a time must be executed by the
+    ///   GPU. In other words, you must submit one, wait for the fence to be signaled, then reset
+    ///   the fence, and then only submit the second.
+    ///
+    /// - If you submit this builder, the fence must be kept alive until it is signaled by the GPU.
+    ///   Destroying the fence earlier is an undefined behavior.
+    ///
+    /// - The fence, command buffers, and semaphores must all belong to the same device.
+    ///
     #[inline]
     pub unsafe fn set_fence_signal(&mut self, fence: &'a Fence) {
-        self.fence = fence.internal_object()
+        self.fence = fence.internal_object();
     }
 
     /// Adds a semaphore to be waited upon before the command buffers are executed.
     ///
     /// Only the given `stages` of the command buffers added afterwards will wait upon
     /// the semaphore. Other stages not included in `stages` can execute before waiting.
+    ///
+    /// # Safety
+    ///
+    /// - The stages must be supported by the device.
+    ///
+    /// - If you submit this builder, the semaphore must be kept alive until you are guaranteed
+    ///   that the GPU has at least started executing the command buffers.
+    ///
+    /// - If you submit this builder, no other queue must be waiting on these semaphores. In other
+    ///   words, each semaphore signal can only correspond to one semaphore wait.
+    ///
+    /// - If you submit this builder, the semaphores must be signaled when the queue execution
+    ///   reaches this submission, or there must be one or more submissions in queues that are
+    ///   going to signal these semaphores. In other words, you must not block the queue with
+    ///   semaphores that can't get signaled.
+    ///
+    /// - The fence, command buffers, and semaphores must all belong to the same device.
+    ///
     #[inline]
     pub unsafe fn add_wait_semaphore(&mut self, semaphore: &'a Semaphore, stages: PipelineStages) {
         debug_assert!(Into::<vk::PipelineStageFlagBits>::into(stages) != 0);
+        // TODO: debug assert that the device supports the stages
         self.wait_semaphores.push(semaphore.internal_object());
         self.dest_stages.push(stages.into());
     }
@@ -81,6 +152,19 @@ impl<'a> SubmitCommandBufferBuilder<'a> {
     /// Adds a command buffer that is executed as part of this command.
     ///
     /// The command buffers are submitted in the order in which they are added.
+    ///
+    /// # Safety
+    ///
+    /// - If you submit this builder, the command buffer must be kept alive until you are
+    ///   guaranteed that the GPU has finished executing it.
+    ///
+    /// - Any calls to vkCmdSetEvent, vkCmdResetEvent or vkCmdWaitEvents that have been recorded
+    ///   into the command buffer must not reference any VkEvent that is referenced by any of
+    ///   those commands that is pending execution on another queue.
+    ///   TODO: rephrase ^ ?
+    ///
+    /// - The fence, command buffers, and semaphores must all belong to the same device.
+    ///
     #[inline]
     pub unsafe fn add_command_buffer<P>(&mut self, command_buffer: &'a UnsafeCommandBuffer<P>)
         where P: CommandPool
@@ -89,18 +173,35 @@ impl<'a> SubmitCommandBufferBuilder<'a> {
     }
 
     /// Returns the number of semaphores to signal.
+    ///
+    /// In other words, this is the number of times `add_signal_semaphore` has been called.
     #[inline]
     pub fn num_signal_semaphores(&self) -> usize {
         self.signal_semaphores.len()
     }
 
     /// Adds a semaphore that is going to be signaled at the end of the submission.
+    ///
+    /// # Safety
+    ///
+    /// - If you submit this builder, the semaphore must be kept alive until you are guaranteed
+    ///   that the GPU has finished executing this submission.
+    ///
+    /// - The semaphore must be in the unsignaled state when queue execution reaches this
+    ///   submission.
+    ///
+    /// - The fence, command buffers, and semaphores must all belong to the same device.
+    ///
     #[inline]
     pub unsafe fn add_signal_semaphore(&mut self, semaphore: &'a Semaphore) {
         self.signal_semaphores.push(semaphore.internal_object());
     }
 
-    /// Submits the command buffer.
+    /// Submits the command buffer to the given queue.
+    ///
+    /// > **Note**: This is an expensive operation, so you may want to merge as many builders as
+    /// > possible together and avoid submitting them one by one.
+    ///
     pub fn submit(mut self, queue: &Queue) -> Result<(), SubmitCommandBufferError> {
         unsafe {
             let vk = queue.device().pointers();
@@ -130,12 +231,13 @@ impl<'a> SubmitCommandBufferBuilder<'a> {
     /// # Panic
     ///
     /// Panics if both builders have a fence already set.
+    // TODO: create multiple batches instead
     pub fn merge(mut self, other: Self) -> Self {
         assert!(self.fence == 0 || other.fence == 0,
                "Can't merge two queue submits that both have a fence");
 
         self.wait_semaphores.extend(other.wait_semaphores);
-        self.dest_stages.extend(other.dest_stages);     // TODO: meh?
+        self.dest_stages.extend(other.dest_stages);     // TODO: meh? will be solved if we submit multiple batches
         self.signal_semaphores.extend(other.signal_semaphores);
         self.command_buffers.extend(other.command_buffers);
 
@@ -197,22 +299,64 @@ impl From<Error> for SubmitCommandBufferError {
 
 #[cfg(test)]
 mod tests {
+    use std::thread;
+    use std::time::Duration;
     use super::*;
     use sync::Fence;
 
     #[test]
+    fn empty_submit() {
+        let (device, queue) = gfx_dev_and_queue!();
+        let mut builder = SubmitCommandBufferBuilder::new();
+        builder.submit(&queue).unwrap();
+    }
+
+    #[test]
+    fn signal_fence() {
+        unsafe {
+            let (device, queue) = gfx_dev_and_queue!();
+
+            let fence = Fence::new(device.clone()).unwrap();
+            assert!(!fence.ready().unwrap());
+
+            let mut builder = SubmitCommandBufferBuilder::new();
+            builder.set_fence_signal(&fence);
+
+            builder.submit(&queue).unwrap();
+            fence.wait(Duration::from_secs(10)).unwrap();
+            assert!(fence.ready().unwrap());
+        }
+    }
+
+    #[test]
+    fn has_fence() {
+        unsafe {
+            let (device, queue) = gfx_dev_and_queue!();
+
+            let fence = Fence::new(device.clone()).unwrap();
+
+            let mut builder = SubmitCommandBufferBuilder::new();
+            assert!(!builder.has_fence());
+            builder.set_fence_signal(&fence);
+            assert!(builder.has_fence());
+        }
+    }
+
+    #[test]
     #[should_panic(expected = "Can't merge two queue submits that both have a fence")]
     fn merge_both_have_fences() {
-        let (device, _) = gfx_dev_and_queue!();
+        unsafe {
+            let (device, _) = gfx_dev_and_queue!();
 
-        let fence1 = Fence::new(device.clone()).unwrap();
-        let fence2 = Fence::new(device.clone()).unwrap();
-        
-        let mut builder1 = SubmitCommandBufferBuilder::new();
-        unsafe { builder1.set_fence_signal(&fence1); }
-        let mut builder2 = SubmitCommandBufferBuilder::new();
-        unsafe { builder2.set_fence_signal(&fence2); }
+            let fence1 = Fence::new(device.clone()).unwrap();
+            let fence2 = Fence::new(device.clone()).unwrap();
 
-        let _ = builder1.merge(builder2);
+            let mut builder1 = SubmitCommandBufferBuilder::new();
+            builder1.set_fence_signal(&fence1);
+            let mut builder2 = SubmitCommandBufferBuilder::new();
+            builder2.set_fence_signal(&fence2);
+
+            let _ = builder1.merge(builder2);
+        }
     }
 }
