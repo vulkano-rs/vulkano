214164bf01cdb6665a6751014d8c92af83022bc8
Samuel Sleight <samuel.sleight@gmail.com>
2017-06-05 18:45:01 +0100

    Added support for #[src=filename]

--- vulkano-shader-derive/src/lib.rs
+++ vulkano-shader-derive/src/lib.rs
@@ -3,20 +3,37 @@ extern crate proc_macro;
 extern crate syn;
 extern crate vulkano_shaders;
 
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(VulkanoShader, attributes(src, ty))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let syn_item = syn::parse_macro_input(&input.to_string()).unwrap();
 
-    let src = syn_item.attrs.iter().filter_map(|attr| {
-        match attr.value {
-            syn::MetaItem::NameValue(ref i, syn::Lit::Str(ref val, _)) if i == "src" => {
-                Some(val.clone())
-            },
-            _ => None
+    let src = {
+        let src = syn_item.attrs.iter().filter_map(|attr| {
+            match attr.value {
+                syn::MetaItem::NameValue(ref i, syn::Lit::Str(ref val, _)) if i == "src" => {
+                    Some(val.clone())
+                },
+                _ => None
+            }
+        }).next().expect("Can't find `src` attribute ; put #[src = \"...\"] for example.");
+
+        if Path::new(&src[..]).is_file() {
+            let mut buf = String::new();
+            File::open(src).and_then(|mut file| file.read_to_string(&mut buf)).expect("Unable to read source from given file");
+            buf
+        } else {
+            src
         }
-    }).next().expect("Can't find `src` attribute ; put #[src = \"...\"] for example.");
+    };
+
+    println!("Source: {}", src);
+
 
     let ty_str = syn_item.attrs.iter().filter_map(|attr| {
         match attr.value {
