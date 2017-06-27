602ccfce989fdc8dfc03ccaf67e1c72d425d5f62
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-06-27 09:55:17 +0200

    copy_buffer is now type-safe

--- vulkano/src/command_buffer/validity/mod.rs
+++ vulkano/src/command_buffer/validity/mod.rs
@@ -9,7 +9,7 @@
 
 //! Functions that check the validity of commands.
 
-pub use self::copy_buffer::{CheckCopyBufferError, check_copy_buffer};
+pub use self::copy_buffer::{CheckCopyBufferError, check_copy_buffer, CheckCopyBuffer};
 pub use self::dynamic_state::{CheckDynamicStateValidityError, check_dynamic_state_validity};
 pub use self::fill_buffer::{CheckFillBufferError, check_fill_buffer};
 pub use self::update_buffer::{CheckUpdateBufferError, check_update_buffer};
