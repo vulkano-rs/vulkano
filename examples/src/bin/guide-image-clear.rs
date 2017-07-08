bceafa2b03589b185b0b517e66d49a6a295c5159
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-07-08 11:13:32 +0200

    Add the example for the next section of the guide

--- examples/src/bin/guide-image-clear.rs
+++ examples/src/bin/guide-image-clear.rs
@@ -7,27 +7,29 @@
 // notice may not be copied, modified, or distributed except
 // according to those terms.
 
-//! This example contains the source code of the second part of the guide at http://vulkano.rs.
+//! This example contains the source code of the third part of the guide at http://vulkano.rs.
 //!
 //! It is not commented, as the explanations can be found in the guide itself.
 
-#[macro_use]
+extern crate image;
 extern crate vulkano;
-#[macro_use]
-extern crate vulkano_shader_derive;
 
-use std::sync::Arc;
+use image::ImageBuffer;
+use image::Rgba;
 use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::command_buffer::AutoCommandBufferBuilder;
 use vulkano::command_buffer::CommandBuffer;
 use vulkano::device::Device;
 use vulkano::device::DeviceExtensions;
+use vulkano::format::ClearValue;
+use vulkano::format::Format;
+use vulkano::image::Dimensions;
+use vulkano::image::StorageImage;
 use vulkano::instance::Features;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
-use vulkano::pipeline::ComputePipeline;
 use vulkano::sync::GpuFuture;
 
 fn main() {
@@ -47,56 +49,27 @@ fn main() {
 
     let queue = queues.next().unwrap();
 
-    // Introduction to compute operations
-    let data_iter = 0 .. 65536;
-    let data_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
-                                                    Some(queue.family()),
-                                                    data_iter).expect("failed to create buffer");
+    // Image creation
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
 
-    // Compute pipelines
-    mod cs {
-        #[derive(VulkanoShader)]
-        #[ty = "compute"]
-        #[src = "
-    #version 450
+    // Clearing an image
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                             Some(queue.family()),
+                                             (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+                                             .expect("failed to create buffer");
 
-    layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-
-    layout(set = 0, binding = 0) buffer Data {
-        uint data[];
-    } data;
-
-    void main() {
-        uint idx = gl_GlobalInvocationID.x;
-        data.data[idx] *= 12;
-    }"
-        ]
-        struct Dummy;
-    }
-
-    let shader = cs::Shader::load(device.clone())
-        .expect("failed to create shader module");
-    let compute_pipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
-        .expect("failed to create compute pipeline"));
-
-    // Descriptor sets
-    let set = Arc::new(simple_descriptor_set!(compute_pipeline.clone(), 0, {
-        data: data_buffer.clone()
-    }));
-
-    // Dispatch
     let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
-        .dispatch([1024, 1, 1], compute_pipeline.clone(), set.clone(), ()).unwrap()
+        .clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0])).unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
         .build().unwrap();
 
     let finished = command_buffer.execute(queue.clone()).unwrap();
     finished.then_signal_fence_and_flush().unwrap()
         .wait(None).unwrap();
 
-    let content = data_buffer.read().unwrap();
-    for (n, val) in content.iter().enumerate() {
-        assert_eq!(*val, n as u32 * 12);
-    }
-
-    println!("Everything succeeded!");
+    // Exporting the result
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
 }
