12157910691afa23c21d289bf6b500f2b85d0945
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-10-07 16:42:23 +0200

    Changed `GraphicsPipelineBuilder` to default to a buffer-less vertex input

--- vulkano/src/pipeline/graphics_pipeline/builder.rs
+++ vulkano/src/pipeline/graphics_pipeline/builder.rs
@@ -43,7 +43,7 @@ use pipeline::shader::GraphicsEntryPointAbstract;
 use pipeline::shader::GraphicsShaderType;
 use pipeline::shader::ShaderInterfaceDefMatch;
 use pipeline::shader::SpecializationConstants;
-use pipeline::vertex::SingleBufferDefinition;
+use pipeline::vertex::BufferlessDefinition;
 use pipeline::vertex::VertexDefinition;
 use pipeline::viewport::Scissor;
 use pipeline::viewport::Viewport;
@@ -81,7 +81,7 @@ struct TessInfo<Tcs, Tcss, Tes, Tess> {
 }
 
 impl
-    GraphicsPipelineBuilder<SingleBufferDefinition<()>,
+    GraphicsPipelineBuilder<BufferlessDefinition,
                             EmptyEntryPointDummy,
                             (),
                             EmptyEntryPointDummy,
@@ -97,7 +97,7 @@ impl
     pub(super) fn new() -> Self {
         unsafe {
             GraphicsPipelineBuilder {
-                vertex_input: SingleBufferDefinition::new(), // TODO: should be empty attrs instead
+                vertex_input: BufferlessDefinition,
                 vertex_shader: None,
                 input_assembly: InputAssembly::triangle_list(),
                 tessellation: None,
