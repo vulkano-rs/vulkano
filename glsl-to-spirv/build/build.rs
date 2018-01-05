50b34d673d3a39f5c7e23421a48dcbfaa18596d4
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-01-05 08:34:26 +0200

    Refactor `build.rs` to work with multiple tools

--- glsl-to-spirv/build/build.rs
+++ glsl-to-spirv/build/build.rs
@@ -2,20 +2,25 @@ extern crate cmake;
 
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
-    println!("cargo:rerun-if-changed=build/glslangValidator.exe");
+    // Determine if we are on Windows and cache the result.
+    let on_windows = {
+        let target = env::var("TARGET").unwrap();
+        target.contains("windows")
+    };
 
-    let target = env::var("TARGET").unwrap();
-    let out_file = Path::new(&env::var("OUT_DIR").unwrap()).join("glslang_validator");
+    // A list of all tools we need to build.
+    // Format: (<tool name>, <tool Git subdirectory>, <path to binary file>)
+    let tools = [
+        ("glslangValidator", "glslang", Path::new("bin/glslangValidator")),
+        ("spirv-opt", "spirv-tools", Path::new("bin/spirv-opt")),
+    ];
 
-    let path = if target.contains("windows") {
-        // TODO: check the hash of the file to make sure that it is not altered
-        Path::new("build/glslangValidator.exe").to_owned()
-
-    } else {
+    // Update all Git submodules at once.
+    if !on_windows {
         // Try to initialize submodules. Don't care if it fails, since this code also runs for
         // the crates.io package.
         let _ = Command::new("git")
@@ -23,11 +28,40 @@ fn main() {
             .arg("update")
             .arg("--init")
             .status();
-        cmake::build("glslang");
-        Path::new(&env::var("OUT_DIR").unwrap())
-            .join("bin")
-            .join("glslangValidator")
-    };
+    }
 
-    fs::copy(&path, &out_file).expect("failed to copy executable");
+    // Cache the output dir, since it's used in multiple places.
+    let out_dir = PathBuf::from(&env::var("OUT_DIR").unwrap());
+
+    // Process all tools.
+    for tool in tools.iter() {
+        // The name of the tool binary.
+        let file = tool.0;
+
+        // Path to Windows pre-built binary.
+        let exe_file = format!("build/{}.exe", file);
+
+        // Rebuild if tool binary is updated.
+        // This will also lead to Git updating the submodules.
+        println!("cargo:rerun-if-changed={}", exe_file);
+
+        let path = if on_windows {
+            // TODO: check the hash of the file to make sure that it is not altered
+            Path::new(&exe_file).to_owned()
+        } else {
+            // Determine the name of the CMake project to build.
+            let project_name = tool.1;
+            cmake::build(project_name);
+
+            // Determine the subpath where the tool binary is.
+            let bin_path = tool.2;
+            out_dir.join(bin_path)
+        };
+
+        // Determine the final file to copy to.
+        let out_file = out_dir.join(file);
+
+        // Copy the binary to the final destination.
+        fs::copy(&path, &out_file).expect("failed to copy executable");
+    }
 }
