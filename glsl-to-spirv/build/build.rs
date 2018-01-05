653d1f3a4aaa613c483033a4dbd0e301d5315352
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-01-05 12:21:13 +0200

    Refactor build script

--- glsl-to-spirv/build/build.rs
+++ glsl-to-spirv/build/build.rs
@@ -6,17 +6,25 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 // A tool used by `glsl-to-spirv`.
-struct Tool<'a> {
-    name: &'a str,
-    builder: &'a Fn(),
-    bin_path: &'a Path,
+trait Tool {
+    // Name of binary.
+    fn name(&self) -> &str;
+
+    // Directory with source files.
+    fn src_dir(&self) -> &Path;
+    // Subpath to the binary, relative to build directory.
+    fn bin_path(&self) -> &Path;
+
+    // Builds the tool.
+    fn build(&self);
 }
 
-fn create_cmake_config(out_dir: &Path, tool_name: &str) -> cmake::Config {
-    let mut cfg = cmake::Config::new(tool_name);
+// Tools which are built using CMake should use this function.
+fn cmake_config(src_dir: &Path) -> cmake::Config {
+    let mut cfg = cmake::Config::new(src_dir);
 
     // Create a different output dir for each tool.
-    let out_dir = out_dir.join(tool_name);
+    let out_dir = Path::new(&env::var("OUT_DIR").unwrap()).join(src_dir);
     fs::create_dir_all(&out_dir).unwrap();
 
     cfg.out_dir(out_dir);
@@ -27,11 +35,55 @@ fn create_cmake_config(out_dir: &Path, tool_name: &str) -> cmake::Config {
     cfg
 }
 
+// The glslangValidator is the reference GLSL compiler.
+struct GlslangValidator;
+
+impl Tool for GlslangValidator {
+    fn name(&self) -> &str {
+        "glslangValidator"
+    }
+
+    fn src_dir(&self) -> &Path {
+        Path::new("glslang")
+    }
+
+    fn bin_path(&self) -> &Path {
+        Path::new("bin/glslangValidator")
+    }
+
+    fn build(&self) {
+        cmake_config(self.src_dir()).build();
+    }
+}
+
+// The spirv-opt tool is used to optimize the generated shader code.
+struct SpirvOpt;
+
+impl Tool for SpirvOpt {
+    fn name(&self) -> &str {
+        "spirv-opt"
+    }
+
+    fn src_dir(&self) -> &Path {
+        Path::new("spirv-tools")
+    }
+
+    fn bin_path(&self) -> &Path {
+        Path::new("bin/spirv-opt")
+    }
+
+    fn build(&self) {
+        let spirv_headers = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("spirv-headers");
+
+        cmake_config(self.src_dir())
+            .define("SPIRV-Headers_SOURCE_DIR", spirv_headers)
+            .build();
+    }
+}
+
 fn main() {
     // Cache the output dir, since it's used in multiple places.
     let out_dir = PathBuf::from(&env::var("OUT_DIR").unwrap());
-
-    let manifest_dir = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
 
     // Determine if we are on Windows and cache the result.
     let on_windows = {
@@ -40,23 +92,9 @@ fn main() {
     };
 
     // A list of all tools we need to build.
-    let tools = [
-        // The glslangValidator is the reference GLSL compiler.
-        Tool {
-            name: "glslangValidator",
-            builder: &|| { create_cmake_config(&out_dir, "glslang").build(); },
-            bin_path: Path::new("glslang/bin/glslangValidator"),
-        },
-        // The spirv-opt tool is used to optimize the generated shader code.
-        Tool {
-            name: "spirv-opt",
-            builder: &|| {
-                create_cmake_config(&out_dir, "spirv-tools")
-                    .define("SPIRV-Headers_SOURCE_DIR", manifest_dir.join("spirv-headers"))
-                    .build();
-            },
-            bin_path: Path::new("spirv-tools/bin/spirv-opt"),
-        },
+    let tools: &[&Tool] = &[
+        &GlslangValidator,
+        &SpirvOpt,
     ];
 
     // Update all Git submodules at once.
@@ -73,7 +111,7 @@ fn main() {
     // Process all tools.
     for tool in tools.iter() {
         // The name of the tool binary.
-        let file = tool.name;
+        let file = tool.name();
 
         // Path to Windows pre-built binary.
         let exe_file = format!("build/{}.exe", file);
@@ -86,11 +124,11 @@ fn main() {
             // TODO: check the hash of the file to make sure that it is not altered
             Path::new(&exe_file).to_owned()
         } else {
-            // Build the tool.
-            (tool.builder)();
+            // Compile the tool.
+            tool.build();
 
             // Determine the subpath where the tool binary is.
-            out_dir.join(tool.bin_path)
+            out_dir.join(tool.src_dir()).join(tool.bin_path())
         };
 
         // Determine the final file to copy to.
