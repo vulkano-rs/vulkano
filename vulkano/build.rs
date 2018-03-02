54e97ae07de4684d02964195dd003e18e57f39f5
Pirh <claire.harris.000@gmail.com>
2018-03-02 11:15:07 +0000

    Link IOKit to fix compilation on macOS against open-source MoltenVK (#926)
    Required by the following imports: https://github.com/KhronosGroup/MoltenVK/blob/93c524d2f05d7a7f983354b8e53cd4e6812708c7/MoltenVK/MoltenVK/Utility/MVKOSExtensions.mm#L31-L32

--- vulkano/build.rs
+++ vulkano/build.rs
@@ -15,6 +15,7 @@ fn main() {
         println!("cargo:rustc-link-search=framework=/Library/Frameworks/");
         println!("cargo:rustc-link-lib=c++");
         println!("cargo:rustc-link-lib=framework=MoltenVK");
+        println!("cargo:rustc-link-lib=framework=IOKit");
         println!("cargo:rustc-link-lib=framework=IOSurface");
         println!("cargo:rustc-link-lib=framework=QuartzCore");
         println!("cargo:rustc-link-lib=framework=Metal");
