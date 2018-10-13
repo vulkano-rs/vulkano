51449afce564613d0cb2829300471ba7a3cbb69e
Aleksi Juvani <aleksi@aleksijuvani.com>
2018-10-13 16:19:58 +0300

    Fix `GpuAccess::increase_lock()` and `unlock()` logic

--- vulkano/src/buffer/gpu_access.rs
+++ vulkano/src/buffer/gpu_access.rs
@@ -112,28 +112,59 @@ impl GpuAccess {
 
     #[inline]
     pub unsafe fn increase_lock(&mut self, range: Range<usize>) {
-        let access = self.ranges
-            .iter_mut()
-            .find(|x| x.range == range)
-            .expect("Tried to increase lock for a buffer range that is not locked");
+        // When a lock is increased, it's possible that the range is not exactly
+        // the same that was used to create the lock. This can happen, for
+        // example when using a single copy_buffer command to copy both vertex
+        // and index data into the buffer. This will result in a single lock
+        // containing the both vertex and index buffer ranges.
 
-        debug_assert!(access.locks >= 1);
-        access.locks += 1;
+        // Later, when draw_indexed is invoked, bind_index_buffer is called, and
+        // it will try to increase the lock with its range, but that range is in
+        // fact a subrange of the range that was used to create the lock.
+
+        // Hence, we will look for an intersecting range and increase the lock
+        // for that instead.
+
+        let mut intersecting_ranges = 0;
+
+        for access in self.ranges
+            .iter_mut()
+            .filter(|x| ranges_intersect(&x.range, &range)) {
+            debug_assert!(access.locks >= 1);
+            access.locks += 1;
+
+            intersecting_ranges += 1;
+        }
+
+        assert!(intersecting_ranges > 0, "Tried to increase lock for a buffer range that is not locked");
+
+        // If multiple intersecting ranges are found, I think that means that an
+        // invalid range was provided here?
+
+        assert!(intersecting_ranges == 1);
     }
 
     #[inline]
     pub unsafe fn unlock(&mut self, range: Range<usize>) {
-        let (i, _) = self.ranges
+        let mut intersecting_ranges = 0;
+
+        for (i, access) in self.ranges
             .iter_mut()
             .enumerate()
-            .find(|(_, x)| x.range == range)
-            .expect("Tried to unlock a buffer range that isn't locked");
+            .filter(|(_, x)| ranges_intersect(&x.range, &range)) {
+            assert!(access.locks >= 1);
+            access.locks -= 1;
 
-        assert!(self.ranges[i].locks >= 1);
-        self.ranges[i].locks -= 1;
-
-        if self.ranges[i].locks == 0 {
-            self.ranges.remove(i);
+            intersecting_ranges += 1;
         }
+
+        assert!(intersecting_ranges > 0, "Tried to unlock a buffer range that isn't locked");
+
+        // If multiple intersecting ranges are found, I think that means that an
+        // invalid range was provided here?
+
+        assert!(intersecting_ranges == 1);
+
+        self.ranges.retain(|x| x.locks > 0);
     }
 }
