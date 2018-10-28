b78ed7f9387528bb7e887e3e5374b3f3faebbcc3
Jonathansty <jonathansty@users.noreply.github.com>
2018-10-28 07:17:25 +0000

    Updating match arm

--- vulkano/src/descriptor/descriptor_set/collection.rs
+++ vulkano/src/descriptor/descriptor_set/collection.rs
@@ -84,12 +84,8 @@ unsafe impl<T> DescriptorSetsCollection for Vec<T>
 
     #[inline]
     fn num_bindings_in_set(&self, set: usize) -> Option<usize> {
-        match self.get(set) {
-            Some(set) => Some(set.num_bindings()),
-            None => None,
-        }
+        self.get(set).map(|x| x.num_bindings())
     }
-
     #[inline]
     fn descriptor(&self, set: usize, binding: usize) -> Option<DescriptorDesc> {
         match self.get(set) {
