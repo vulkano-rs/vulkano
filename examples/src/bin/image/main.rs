1eadd981907a7167f7deef507bf47eeb48c5b69e
Hibiki KOMINAMI <depkey98@gmail.com>
2019-10-03 20:14:43 +0900

    replaced immidiate dimensions value to dynamic (#1238)

--- examples/src/bin/image/main.rs
+++ examples/src/bin/image/main.rs
@@ -114,11 +114,12 @@ fn main() {
     let (texture, tex_future) = {
         let image = image::load_from_memory_with_format(include_bytes!("image_img.png"),
             ImageFormat::PNG).unwrap().to_rgba();
+        let dimensions = Dimensions::Dim2d { width: image.width(), height: image.height() };
         let image_data = image.into_raw().clone();
 
         ImmutableImage::from_iter(
             image_data.iter().cloned(),
-            Dimensions::Dim2d { width: 93, height: 93 },
+            dimensions,
             Format::R8G8B8A8Srgb,
             queue.clone()
         ).unwrap()
