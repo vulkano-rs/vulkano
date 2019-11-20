1fc612592b2d2b9aa8d2cb8a856a14d6e9ef54e5
James Lee <jbit@jbit.net>
2019-11-21 10:25:56 +1100

    Add unit test to ensure `DebugCallback` can be sent between threads

--- vulkano/src/instance/debug.rs
+++ vulkano/src/instance/debug.rs
@@ -340,3 +340,21 @@ impl From<Error> for DebugCallbackCreationError {
         panic!("unexpected error: {:?}", err)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+    #[test]
+    fn ensure_sendable() {
+        // It's useful to be able to initialize a DebugCallback on one thread
+        // and keep it alive on another thread.
+        let instance = instance!();
+        let severity = MessageSeverity::none();
+        let ty = MessageType::all();
+        let callback = DebugCallback::new(&instance, severity, ty, |_| {});
+        thread::spawn(move || {
+            let _ = callback;
+        });
+    }
+}
