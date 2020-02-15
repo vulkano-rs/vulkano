205e2b385a711b899b54ead979fff4938d84a0e3
José Miguel Sánchez García <soy.jmi2k@gmail.com>
2020-02-15 16:51:21 +0100

    Add vulkano-derive with custom derive for Vertex

---
+++ vulkano-derive/src/lib.rs
@@ -0,0 +1,62 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, DataStruct, DeriveInput, Fields};
+
+#[proc_macro_derive(Vertex)]
+pub fn vertex_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input!(input as DeriveInput);
+
+    let fields = match &ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => &fields.named,
+        _ => panic!("expected struct with named fields"),
+    };
+
+    let member = fields.iter().map(|field| &field.ident);
+    let name = &ast.ident;
+
+    let output = quote! {
+        #[allow(unsafe_code)]
+        unsafe impl ::vulkano::pipeline::vertex::Vertex for #name {
+            #[inline(always)]
+            fn member(name: &str)
+            -> Option<::vulkano::pipeline::vertex::VertexMemberInfo> {
+                use std::ptr;
+
+                #[allow(unused_imports)]
+                use ::vulkano::format::Format;
+                use ::vulkano::pipeline::vertex::VertexMemberInfo;
+                use ::vulkano::pipeline::vertex::VertexMemberTy;
+                use ::vulkano::pipeline::vertex::VertexMember;
+
+                #(
+                    if name == stringify!(#member) {
+                        let dummy = <#name>::default();
+                        #[inline] fn f<T: VertexMember>(_: &T)
+                        -> (VertexMemberTy, usize) {
+                            T::format()
+                        }
+
+                        let (ty, array_size) = f(&dummy.#member);
+                        let dummy_ptr = (&dummy) as *const _;
+                        let member_ptr = (&dummy.#member) as *const _;
+
+                        return Some(VertexMemberInfo {
+                            offset: member_ptr as usize - dummy_ptr as usize,
+                            ty: ty,
+                            array_size: array_size,
+                        })
+                    }
+                )*
+
+                None
+            }
+        }
+    };
+
+    output.into()
+}
