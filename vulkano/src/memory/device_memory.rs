vulkano-rs/vulkano

commit 2d60c08c10bb40b0cfe7bbfb73b464e499cb55a5

Initial YUV and external memory support (#1467)
* vulkano: image: improve formatting

Ran cargo fmt --all.  Should we use clippy too ?

* vk_sys: add additional formats

This updates the vk formats enum to the header that's on my
system (vk 1.2-ish).

* vulkano: image: Add NV12 and YV12 support

These formats are commonly used as targets for hardware and
software video decode.  The common case is the swapchain allocator
(gralloc in the Android use case) allocates some memory, the video
stack decodes to it, and then memory can be composited by Vulkan
or sent directly to the display.

* vk_sys: update structure types

This updates the structure types to a VK 1.2-ish state.  Long term,
it makes a ton of sense to autogenerate vk-sys to make adding
additional features and enumerations easier [1].  For now, we can
hand edit.

[1] (https://github.com/vulkano-rs/vulkano/issues/89)

* vk_sys: add VK_KHR_external_memory_fd bindings

This adds some basic external memory features.  Most of these
features are core in VK1.1.

* vulkano: memory: add and use DeviceMemoryBuilder

We'll need to:

(a) Create exportable memory
(b) import from a OS descriptor to create DeviceMemory
(c) Also support dedicated allocations

The device memory API is becoming rather complicated.  Let's use
the common builder pattern to simplify this.

* vulkano: memory: implement some external features

This change is sufficient to create exportable memory and
export it.  It's only been tested on Linux platforms and depends
on Unix file descriptors, so enable it only there for now.

Import support will be added later.  Support for external buffers
and images can also be added later if someone needs it.

vulkano/src/memory/device_memory.rs
@@ -17,12 +17,18 @@ use std::os::raw::c_void;
 use std::ptr;
 use std::sync::Arc;
 
+#[cfg(target_os = "linux")]
+use std::fs::File;
+#[cfg(target_os = "linux")]
+use std::os::unix::io::FromRawFd;
+
 use check_errors;
 use device::Device;
 use device::DeviceOwned;
 use instance::MemoryType;
 use memory::Content;
 use memory::DedicatedAlloc;
+use memory::ExternalMemoryHandleType;
 use vk;
 use Error;
 use OomError;
@@ -48,6 +54,206 @@ pub struct DeviceMemory {
     device: Arc<Device>,
     size: usize,
     memory_type_index: u32,
+    handle_types: ExternalMemoryHandleType,
+}
+
+/// Represents a builder for the device memory object.
+///
+/// # Example
+///
+/// ```
+/// use vulkano::memory::DeviceMemoryBuilder;
+///
+/// # let device: std::sync::Arc<vulkano::device::Device> = return;
+/// let mem_ty = device.physical_device().memory_types().next().unwrap();
+///
+/// // Allocates 1KB of memory.
+/// let memory = DeviceMemoryBuilder::new(device, mem_ty, 1024).build().unwrap();
+/// ```
+pub struct DeviceMemoryBuilder<'a> {
+    device: Arc<Device>,
+    memory_type: MemoryType<'a>,
+    allocate: vk::MemoryAllocateInfo,
+    dedicated_info: Option<vk::MemoryDedicatedAllocateInfoKHR>,
+    export_info: Option<vk::ExportMemoryAllocateInfo>,
+    import_info: Option<vk::ImportMemoryFdInfoKHR>,
+    handle_types: ExternalMemoryHandleType,
+}
+
+impl<'a> DeviceMemoryBuilder<'a> {
+    /// Returns a new `DeviceMemoryBuilder` given the required device, memory type and size fields.
+    ///
+    /// # Panic
+    ///
+    /// - Panics if `size` is 0.
+    /// - Panics if `memory_type` doesn't belong to the same physical device as `device`.
+    pub fn new(device: Arc<Device>, memory_type: MemoryType, size: usize) -> DeviceMemoryBuilder {
+        assert!(size > 0);
+        assert_eq!(
+            device.physical_device().internal_object(),
+            memory_type.physical_device().internal_object()
+        );
+
+        let allocate = vk::MemoryAllocateInfo {
+            sType: vk::STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+            pNext: ptr::null(),
+            allocationSize: size as u64,
+            memoryTypeIndex: memory_type.id(),
+        };
+
+        DeviceMemoryBuilder {
+            device,
+            memory_type,
+            allocate,
+            dedicated_info: None,
+            export_info: None,
+            import_info: None,
+            handle_types: ExternalMemoryHandleType::none(),
+        }
+    }
+
+    /// Sets an optional field for dedicated allocations in the `DeviceMemoryBuilder`.  To maintain
+    /// backwards compatibility, this function does nothing when dedicated allocation has not been
+    /// enabled on the device.
+    ///
+    /// # Panic
+    ///
+    /// - Panics if the dedicated allocation info has already been set.
+    pub fn dedicated_info(mut self, dedicated: DedicatedAlloc<'a>) -> DeviceMemoryBuilder {
+        assert!(self.dedicated_info.is_none());
+
+        if self.device.loaded_extensions().khr_dedicated_allocation {
+            self.dedicated_info = match dedicated {
+                DedicatedAlloc::Buffer(buffer) => Some(vk::MemoryDedicatedAllocateInfoKHR {
+                    sType: vk::STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO_KHR,
+                    pNext: ptr::null(),
+                    image: 0,
+                    buffer: buffer.internal_object(),
+                }),
+                DedicatedAlloc::Image(image) => Some(vk::MemoryDedicatedAllocateInfoKHR {
+                    sType: vk::STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO_KHR,
+                    pNext: ptr::null(),
+                    image: image.internal_object(),
+                    buffer: 0,
+                }),
+                DedicatedAlloc::None => return self,
+            };
+
+            let ptr = self
+                .dedicated_info
+                .as_ref()
+                .map(|i| i as *const vk::MemoryDedicatedAllocateInfoKHR)
+                .unwrap_or(ptr::null()) as *const _;
+
+            if let Some(ref mut export_info) = self.export_info {
+                export_info.pNext = ptr;
+            } else {
+                self.allocate.pNext = ptr;
+            }
+        }
+
+        self
+    }
+
+    /// Sets an optional field for exportable allocations in the `DeviceMemoryBuilder`.
+    ///
+    /// # Panic
+    ///
+    /// - Panics if the export info has already been set.
+    /// - Panics if the extensions associated with `handle_types` have not been loaded by the
+    ///   by the device.
+    pub fn export_info(
+        mut self,
+        handle_types: ExternalMemoryHandleType,
+    ) -> DeviceMemoryBuilder<'a> {
+        assert!(self.export_info.is_none());
+        // TODO: check exportFromImportedHandleTypes instead.
+        assert!(self.import_info.is_none());
+
+        // Only extensions tested with Vulkano so far.
+        assert!(self.device.loaded_extensions().khr_external_memory);
+        assert!(self.device.loaded_extensions().khr_external_memory_fd);
+
+        let handle_bits = handle_types.to_bits();
+        if handle_bits & vk::EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT == 0 {
+            assert!(self.device.loaded_extensions().ext_external_memory_dmabuf);
+        }
+
+        let unsupported = handle_bits
+            & !(vk::EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT
+                | vk::EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT);
+        assert!(unsupported == 0);
+
+        let export_info = vk::ExportMemoryAllocateInfo {
+            sType: vk::STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO,
+            pNext: ptr::null(),
+            handleTypes: handle_bits,
+        };
+
+        self.export_info = Some(export_info);
+        let ptr = self
+            .export_info
+            .as_ref()
+            .map(|i| i as *const vk::ExportMemoryAllocateInfo)
+            .unwrap_or(ptr::null()) as *const _;
+
+        if let Some(ref mut dedicated_info) = self.dedicated_info {
+            dedicated_info.pNext = ptr;
+        } else {
+            self.allocate.pNext = ptr;
+        }
+
+        self.handle_types = handle_types;
+        self
+    }
+
+    /// Creates a `DeviceMemory` object on success, consuming the `DeviceMemoryBuilder`.  An error
+    /// is returned if the requested allocation is too large or if the total number of allocations
+    /// would exceed per-device limits.
+    pub fn build(self) -> Result<DeviceMemory, DeviceMemoryAllocError> {
+        // Note: This check is disabled because MoltenVK doesn't report correct heap sizes yet.
+        // This check was re-enabled because Mesa aborts if `size` is Very Large.
+        //
+        // Conversions won't panic since it's based on `vkDeviceSize`, which is a u64 in the VK
+        // header.  Not sure why we bother with usizes.
+        let reported_heap_size = self.memory_type.heap().size() as u64;
+        if reported_heap_size != 0 && self.allocate.allocationSize > reported_heap_size {
+            return Err(DeviceMemoryAllocError::OomError(
+                OomError::OutOfDeviceMemory,
+            ));
+        }
+
+        let memory = unsafe {
+            let physical_device = self.device.physical_device();
+            let mut allocation_count = self
+                .device
+                .allocation_count()
+                .lock()
+                .expect("Poisoned mutex");
+            if *allocation_count >= physical_device.limits().max_memory_allocation_count() {
+                return Err(DeviceMemoryAllocError::TooManyObjects);
+            }
+            let vk = self.device.pointers();
+
+            let mut output = MaybeUninit::uninit();
+            check_errors(vk.AllocateMemory(
+                self.device.internal_object(),
+                &self.allocate,
+                ptr::null(),
+                output.as_mut_ptr(),
+            ))?;
+            *allocation_count += 1;
+            output.assume_init()
+        };
+
+        Ok(DeviceMemory {
+            memory: memory,
+            device: self.device,
+            size: self.allocate.allocationSize as usize,
+            memory_type_index: self.memory_type.id(),
+            handle_types: self.handle_types,
+        })
+    }
 }
 
 impl DeviceMemory {
@@ -67,7 +273,7 @@ impl DeviceMemory {
         memory_type: MemoryType,
         size: usize,
     ) -> Result<DeviceMemory, DeviceMemoryAllocError> {
-        DeviceMemory::dedicated_alloc(device, memory_type, size, DedicatedAlloc::None)
+        DeviceMemoryBuilder::new(device, memory_type, size).build()
     }
 
     /// Same as `alloc`, but allows specifying a resource that will be bound to the memory.
@@ -84,77 +290,9 @@ impl DeviceMemory {
         size: usize,
         resource: DedicatedAlloc,
     ) -> Result<DeviceMemory, DeviceMemoryAllocError> {
-        assert!(size >= 1);
-        assert_eq!(
-            device.physical_device().internal_object(),
-            memory_type.physical_device().internal_object()
-        );
-
-        // Note: This check is disabled because MoltenVK doesn't report correct heap sizes yet.
-        // This check was re-enabled because Mesa aborts if `size` is Very Large.
-        let reported_heap_size = memory_type.heap().size();
-        if reported_heap_size != 0 && size > reported_heap_size {
-            return Err(DeviceMemoryAllocError::OomError(
-                OomError::OutOfDeviceMemory,
-            ));
-        }
-
-        let memory = unsafe {
-            let physical_device = device.physical_device();
-            let mut allocation_count = device.allocation_count().lock().expect("Poisoned mutex");
-            if *allocation_count >= physical_device.limits().max_memory_allocation_count() {
-                return Err(DeviceMemoryAllocError::TooManyObjects);
-            }
-            let vk = device.pointers();
-
-            // Decide whether we are going to pass a `vkMemoryDedicatedAllocateInfoKHR`.
-            let dedicated_alloc_info = if device.loaded_extensions().khr_dedicated_allocation {
-                match resource {
-                    DedicatedAlloc::Buffer(buffer) => Some(vk::MemoryDedicatedAllocateInfoKHR {
-                        sType: vk::STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO_KHR,
-                        pNext: ptr::null(),
-                        image: 0,
-                        buffer: buffer.internal_object(),
-                    }),
-                    DedicatedAlloc::Image(image) => Some(vk::MemoryDedicatedAllocateInfoKHR {
-                        sType: vk::STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO_KHR,
-                        pNext: ptr::null(),
-                        image: image.internal_object(),
-                        buffer: 0,
-                    }),
-                    DedicatedAlloc::None => None,
-                }
-            } else {
-                None
-            };
-
-            let infos = vk::MemoryAllocateInfo {
-                sType: vk::STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
-                pNext: dedicated_alloc_info
-                    .as_ref()
-                    .map(|i| i as *const vk::MemoryDedicatedAllocateInfoKHR)
-                    .unwrap_or(ptr::null()) as *const _,
-                allocationSize: size as u64,
-                memoryTypeIndex: memory_type.id(),
-            };
-
-            let mut output = MaybeUninit::uninit();
-            check_errors(vk.AllocateMemory(
-                device.internal_object(),
-                &infos,
-                ptr::null(),
-                output.as_mut_ptr(),
-            ))?;
-            *allocation_count += 1;
-            output.assume_init()
-        };
-
-        Ok(DeviceMemory {
-            memory: memory,
-            device: device,
-            size: size,
-            memory_type_index: memory_type.id(),
-        })
+        DeviceMemoryBuilder::new(device, memory_type, size)
+            .dedicated_info(resource)
+            .build()
     }
 
     /// Allocates a chunk of memory and maps it.
@@ -220,6 +358,48 @@ impl DeviceMemory {
     #[inline]
     pub fn size(&self) -> usize {
         self.size
+    }
+
+    /// Exports the device memory into a Unix file descriptor.  The caller retains ownership of the
+    /// file, as per the Vulkan spec.
+    ///
+    /// # Panic
+    ///
+    /// - Panics if the user requests an invalid handle type for this device memory object.
+    #[inline]
+    #[cfg(target_os = "linux")]
+    pub fn export_fd(
+        &self,
+        handle_type: ExternalMemoryHandleType,
+    ) -> Result<File, DeviceMemoryAllocError> {
+        let vk = self.device.pointers();
+
+        let bits = handle_type.to_bits();
+        assert!(
+            bits == vk::EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT
+                || bits == vk::EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT
+        );
+        assert!(handle_type.to_bits() & self.handle_types.to_bits() != 0);
+
+        let fd = unsafe {
+            let info = vk::MemoryGetFdInfoKHR {
+                sType: vk::STRUCTURE_TYPE_MEMORY_GET_FD_INFO_KHR,
+                pNext: ptr::null(),
+                memory: self.memory,
+                handleType: handle_type.to_bits(),
+            };
+
+            let mut output = MaybeUninit::uninit();
+            check_errors(vk.GetMemoryFdKHR(
+                self.device.internal_object(),
+                &info,
+                output.as_mut_ptr(),
+            ))?;
+            output.assume_init()
+        };
+
+        let file = unsafe { File::from_raw_fd(fd) };
+        Ok(file)
     }
 }
 

vulkano/src/device/extensions.rs
@@ -119,6 +119,9 @@ device_extensions! {
     ext_debug_utils => b"VK_EXT_debug_utils",
     khr_multiview => b"VK_KHR_multiview",
     ext_full_screen_exclusive => b"VK_EXT_full_screen_exclusive",
+    khr_external_memory => b"VK_KHR_external_memory",
+    khr_external_memory_fd => b"VK_KHR_external_memory_fd",
+    ext_external_memory_dmabuf => b"VK_EXT_external_memory_dma_buf",
 }
 
 /// This helper type can only be instantiated inside this module.

vulkano/src/format.rs
@@ -206,7 +206,7 @@ macro_rules! formats {
                 }
             }
 
-            /// Returns (width, heigh) of the dimensions for block based formats. For
+            /// Returns (width, height) of the dimensions for block based formats. For
             /// non block formats will return (1,1)
             #[inline]
             pub fn block_dimensions(&self) -> (u32, u32) {
@@ -432,6 +432,27 @@ macro_rules! formats {
         }
     };
 
+    (__inner_impl__ $name:ident ycbcr) => {
+        unsafe impl FormatDesc for $name {
+            type ClearValue = [f32; 4];
+
+	    #[inline]
+            fn format(&self) -> Format {
+                Format::$name
+            }
+
+            #[inline]
+            fn decode_clear_value(&self, val: Self::ClearValue) -> ClearValue {
+                ClearValue::None
+            }
+        }
+
+        unsafe impl PossibleYcbcrFormatDesc for $name {
+            #[inline(always)]
+            fn is_ycbcr(&self) -> bool { true }
+        }
+    };
+
     (__inner_ty__ $name:ident float=$num:tt) => { FormatTy::Float };
     (__inner_ty__ $name:ident uint=$num:tt) => { FormatTy::Uint };
     (__inner_ty__ $name:ident sint=$num:tt) => { FormatTy::Sint };
@@ -439,6 +460,7 @@ macro_rules! formats {
     (__inner_ty__ $name:ident stencil) => { FormatTy::Stencil };
     (__inner_ty__ $name:ident depthstencil) => { FormatTy::DepthStencil };
     (__inner_ty__ $name:ident compressed=$f:tt) => { FormatTy::Compressed };
+    (__inner_ty__ $name:ident ycbcr) => { FormatTy::Ycbcr };
 
 
     (__inner_strongstorage__ $name:ident [$ty:ty; $dim:expr]) => {
@@ -648,6 +670,8 @@ formats! {
     ASTC_12x10SrgbBlock => FORMAT_ASTC_12x10_SRGB_BLOCK [(12, 10)] [Some(16)] [compressed=texture_compression_astc_ldr] {u8},
     ASTC_12x12UnormBlock => FORMAT_ASTC_12x12_UNORM_BLOCK [(12, 12)] [Some(16)] [compressed=texture_compression_astc_ldr] {u8},
     ASTC_12x12SrgbBlock => FORMAT_ASTC_12x12_SRGB_BLOCK [(12, 12)] [Some(16)] [compressed=texture_compression_astc_ldr] {u8},
+    G8B8R8_3PLANE420Unorm => FORMAT_G8_B8_R8_3PLANE_420_UNORM [(1, 1)] [None] [ycbcr] {},
+    G8B8R8_2PLANE420Unorm => FORMAT_G8_B8R8_2PLANE_420_UNORM [(1, 1)] [None] [ycbcr] {},
 }
 
 pub unsafe trait FormatDesc {
@@ -772,6 +796,19 @@ unsafe impl PossibleFloatOrCompressedFormatDesc for Format {
     }
 }
 
+/// Trait for types that can possibly describe a Ycbcr format.
+pub unsafe trait PossibleYcbcrFormatDesc: FormatDesc {
+    /// Trait for types that can possibly describe a Ycbcr format.
+    fn is_ycbcr(&self) -> bool;
+}
+
+unsafe impl PossibleYcbcrFormatDesc for Format {
+    #[inline]
+    fn is_ycbcr(&self) -> bool {
+        self.ty() == FormatTy::Ycbcr
+    }
+}
+
 macro_rules! impl_pixel {
     {$($ty:ty;)+} => {
         $(impl_pixel!(inner $ty);)*
@@ -818,6 +855,7 @@ pub enum FormatTy {
     Stencil,
     DepthStencil,
     Compressed,
+    Ycbcr,
 }
 
 impl FormatTy {
@@ -825,9 +863,7 @@ impl FormatTy {
     #[inline]
     pub fn is_depth_and_or_stencil(&self) -> bool {
         match *self {
-            FormatTy::Depth => true,
-            FormatTy::Stencil => true,
-            FormatTy::DepthStencil => true,
+            FormatTy::Depth | FormatTy::Stencil | FormatTy::DepthStencil => true,
             _ => false,
         }
     }

vulkano/src/image/aspect.rs
@@ -1,0 +1,132 @@
+// Copyright (c) 2020 The vulkano developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
+
+use std::ops::BitOr;
+use vk;
+
+/// Describes how an aspect of the image that be used to query Vulkan.  This is **not** just a suggestion.
+/// Check out VkImageAspectFlagBits in the Vulkan spec.
+///
+/// If you specify an aspect of the image that doesn't exist (for example, depth for a YUV image), a panic
+/// will happen.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct ImageAspect {
+    pub color: bool,
+    pub depth: bool,
+    pub stencil: bool,
+    pub metadata: bool,
+    pub plane0: bool,
+    pub plane1: bool,
+    pub plane2: bool,
+    pub memory_plane0: bool,
+    pub memory_plane1: bool,
+    pub memory_plane2: bool,
+}
+
+impl ImageAspect {
+    /// Builds a `ImageAspect` with all values set to false. Useful as a default value.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use vulkano::image::ImageAspect as ImageAspect;
+    ///
+    /// let _aspect = ImageAspect {
+    ///     color: true,
+    ///     depth: true,
+    ///     .. ImageAspect::none()
+    /// };
+    /// ```
+    #[inline]
+    pub fn none() -> ImageAspect {
+        ImageAspect {
+            color: false,
+            depth: false,
+            stencil: false,
+            metadata: false,
+            plane0: false,
+            plane1: false,
+            plane2: false,
+            memory_plane0: false,
+            memory_plane1: false,
+            memory_plane2: false,
+        }
+    }
+
+    #[inline]
+    pub(crate) fn to_aspect_bits(&self) -> vk::ImageAspectFlagBits {
+        let mut result = 0;
+        if self.color {
+            result |= vk::IMAGE_ASPECT_COLOR_BIT;
+        }
+        if self.depth {
+            result |= vk::IMAGE_ASPECT_DEPTH_BIT;
+        }
+        if self.stencil {
+            result |= vk::IMAGE_ASPECT_STENCIL_BIT;
+        }
+        if self.metadata {
+            result |= vk::IMAGE_ASPECT_METADATA_BIT;
+        }
+        if self.plane0 {
+            result |= vk::IMAGE_ASPECT_PLANE_0_BIT;
+        }
+        if self.plane1 {
+            result |= vk::IMAGE_ASPECT_PLANE_1_BIT;
+        }
+        if self.plane2 {
+            result |= vk::IMAGE_ASPECT_PLANE_2_BIT;
+        }
+        if self.memory_plane0 {
+            result |= vk::IMAGE_ASPECT_MEMORY_PLANE_0_BIT_EXT;
+        }
+        if self.memory_plane1 {
+            result |= vk::IMAGE_ASPECT_MEMORY_PLANE_1_BIT_EXT;
+        }
+        if self.memory_plane2 {
+            result |= vk::IMAGE_ASPECT_MEMORY_PLANE_2_BIT_EXT
+        }
+        result
+    }
+
+    pub(crate) fn from_bits(val: u32) -> ImageAspect {
+        ImageAspect {
+            color: (val & vk::IMAGE_ASPECT_COLOR_BIT) != 0,
+            depth: (val & vk::IMAGE_ASPECT_DEPTH_BIT) != 0,
+            stencil: (val & vk::IMAGE_ASPECT_STENCIL_BIT) != 0,
+            metadata: (val & vk::IMAGE_ASPECT_METADATA_BIT) != 0,
+            plane0: (val & vk::IMAGE_ASPECT_PLANE_0_BIT) != 0,
+            plane1: (val & vk::IMAGE_ASPECT_PLANE_1_BIT) != 0,
+            plane2: (val & vk::IMAGE_ASPECT_PLANE_2_BIT) != 0,
+            memory_plane0: (val & vk::IMAGE_ASPECT_MEMORY_PLANE_0_BIT_EXT) != 0,
+            memory_plane1: (val & vk::IMAGE_ASPECT_MEMORY_PLANE_1_BIT_EXT) != 0,
+            memory_plane2: (val & vk::IMAGE_ASPECT_MEMORY_PLANE_2_BIT_EXT) != 0,
+        }
+    }
+}
+
+impl BitOr for ImageAspect {
+    type Output = Self;
+
+    #[inline]
+    fn bitor(self, rhs: Self) -> Self {
+        ImageAspect {
+            color: self.color || rhs.color,
+            depth: self.depth || rhs.depth,
+            stencil: self.stencil || rhs.stencil,
+            metadata: self.metadata || rhs.metadata,
+            plane0: self.plane0 || rhs.plane0,
+            plane1: self.plane1 || rhs.plane1,
+            plane2: self.plane2 || rhs.plane2,
+            memory_plane0: self.memory_plane0 || rhs.memory_plane0,
+            memory_plane1: self.memory_plane1 || rhs.memory_plane1,
+            memory_plane2: self.memory_plane2 || rhs.memory_plane2,
+        }
+    }
+}

vulkano/src/image/immutable.rs
@@ -66,7 +66,6 @@ pub struct ImmutableImage<F, A = PotentialDedicatedAllocation<StdMemoryPoolAlloc
     layout: ImageLayout,
 }
 
-
 /// Image whose purpose is to access only a part of one image, for any kind of access
 /// We define a part of one image here by a level of mipmap, or a layer of an array
 /// The image attribute must be an implementation of ImageAccess
@@ -80,8 +79,7 @@ pub struct SubImage {
     layout: ImageLayout,
 }
 
-impl SubImage
-{
+impl SubImage {
     pub fn new(
         image: Arc<dyn ImageAccess + Sync + Send>,
         mip_level: u32,
@@ -120,7 +118,7 @@ fn has_mipmaps(mipmaps: MipmapsCount) -> bool {
     match mipmaps {
         MipmapsCount::One => false,
         MipmapsCount::Log2 => true,
-        MipmapsCount::Specific(x) => x > 1
+        MipmapsCount::Specific(x) => x > 1,
     }
 }
 
@@ -422,7 +420,7 @@ impl<F> ImmutableImage<F> {
 
         let future = match cb.execute(queue) {
             Ok(f) => f,
-            Err(e) => unreachable!("{:?}", e)
+            Err(e) => unreachable!("{:?}", e),
         };
 
         image.initialized.store(true, Ordering::Relaxed);
@@ -583,8 +581,7 @@ where
     }
 }
 
-unsafe impl ImageAccess for SubImage
-{
+unsafe impl ImageAccess for SubImage {
     #[inline]
     fn inner(&self) -> ImageInner {
         self.image.inner()

vulkano/src/image/mod.rs
@@ -49,6 +49,7 @@
 use std::cmp;
 use std::convert::TryFrom;
 
+pub use self::aspect::ImageAspect;
 pub use self::attachment::AttachmentImage;
 pub use self::immutable::ImmutableImage;
 pub use self::layout::ImageLayout;
@@ -60,6 +61,7 @@ pub use self::traits::ImageInner;
 pub use self::traits::ImageViewAccess;
 pub use self::usage::ImageUsage;
 
+mod aspect;
 pub mod attachment; // TODO: make private
 pub mod immutable; // TODO: make private
 mod layout;
@@ -684,11 +686,11 @@ impl ImageDimensions {
 
 #[cfg(test)]
 mod tests {
+    use format;
+    use image::Dimensions;
     use image::ImageDimensions;
     use image::ImmutableImage;
-    use image::Dimensions;
     use image::MipmapsCount;
-    use format;
 
     #[test]
     fn max_mipmaps() {
@@ -806,13 +808,23 @@ mod tests {
     fn mipmap_working_immutable_image() {
         let (device, queue) = gfx_dev_and_queue!();
 
-        let dimensions = Dimensions::Dim2d{width: 512, height: 512};
+        let dimensions = Dimensions::Dim2d {
+            width: 512,
+            height: 512,
+        };
         {
             let mut vec = Vec::new();
 
             vec.resize(512 * 512, 0u8);
 
-            let (image, _) = ImmutableImage::from_iter(vec.into_iter(), dimensions, MipmapsCount::One, format::R8Unorm, queue.clone()).unwrap();
+            let (image, _) = ImmutableImage::from_iter(
+                vec.into_iter(),
+                dimensions,
+                MipmapsCount::One,
+                format::R8Unorm,
+                queue.clone(),
+            )
+            .unwrap();
             assert_eq!(image.mipmap_levels(), 1);
         }
         {
@@ -820,7 +832,14 @@ mod tests {
 
             vec.resize(512 * 512, 0u8);
 
-            let (image, _) = ImmutableImage::from_iter(vec.into_iter(), dimensions, MipmapsCount::Log2, format::R8Unorm, queue.clone()).unwrap();
+            let (image, _) = ImmutableImage::from_iter(
+                vec.into_iter(),
+                dimensions,
+                MipmapsCount::Log2,
+                format::R8Unorm,
+                queue.clone(),
+            )
+            .unwrap();
             assert_eq!(image.mipmap_levels(), 10);
         }
     }

commit 8b13bfc2804f9e52d729883adf9e643cea9621f2

vulkano: memory: fix opaque fd support
Don't panic if user doesn't request dma-buf and dma-buf
isn't present on the system.

Fixes: 2d60c08c10bb4 ("Initial YUV and external memory support")

vulkano/src/memory/device_memory.rs
@@ -175,7 +175,7 @@ impl<'a> DeviceMemoryBuilder<'a> {
         assert!(self.device.loaded_extensions().khr_external_memory_fd);
 
         let handle_bits = handle_types.to_bits();
-        if handle_bits & vk::EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT == 0 {
+        if handle_bits & vk::EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT != 0 {
             assert!(self.device.loaded_extensions().ext_external_memory_dmabuf);
         }
 