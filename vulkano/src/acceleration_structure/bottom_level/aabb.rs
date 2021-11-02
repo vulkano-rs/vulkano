13ebe6abdad61bf3e2c6eaa16a5993ffd56d3b12
VictorBulba <victor@lingle.io>
2021-11-02 19:34:49 +0300

    Aabb validation

--- vulkano/src/acceleration_structure/bottom_level/aabb.rs
+++ vulkano/src/acceleration_structure/bottom_level/aabb.rs
@@ -13,14 +13,48 @@ use crate::buffer::TypedBufferAccess;
 use crate::device::Device;
 use crate::VulkanObject;
 use ash::vk::Handle;
+use std::fmt;
 use std::mem::size_of;
 use std::sync::Arc;
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct AabbPosition {
-    pub min: [f32; 3],
-    pub max: [f32; 3],
+    min: [f32; 3],
+    max: [f32; 3],
+}
+
+#[derive(Debug)]
+pub struct IncorrectAabb;
+
+impl fmt::Display for IncorrectAabb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "AABB must not have max that's smaller than min in any dimension"
+        )
+    }
+}
+
+impl std::error::Error for IncorrectAabb {}
+
+impl AabbPosition {
+    pub fn new(min: [f32; 3], max: [f32; 3]) -> Result<AabbPosition, IncorrectAabb> {
+        let [min_x, min_y, min_z] = min;
+        let [max_x, max_y, max_z] = max;
+        if max_x < min_x || max_y < min_y || max_z < min_z {
+            return Err(IncorrectAabb);
+        }
+        Ok(Self { min, max })
+    }
+
+    pub fn min(&self) -> [f32; 3] {
+        self.min
+    }
+
+    pub fn max(&self) -> [f32; 3] {
+        self.max
+    }
 }
 
 fn make_aabb_data(aabb: &dyn BufferAccess) -> ash::vk::AccelerationStructureGeometryAabbsDataKHR {
