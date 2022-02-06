vulkano-rs/vulkano

commit 657012142b59a53b65caebec31c4f4f53aea4b57

Allow `copy_image`, `blit_image` and `copy_buffer` source and destination to be the same image (#1782)
* Allow `copy_image` source and destination to be the same image

The doc: `srcImage and dstImage can be the same image or alias the same
memory`.

This was achieved by locking only one image exclusively if both are the same.

* Allow `blit_image` and `copy_buffer` to have the same src/dest buffers

Regarding this and the previous commits, I noticed an important note in
the docs.
`regions must not overlap`

Before this, it was not important check for us to do, since we can't
have overlapping regions if both buffers are different. But now we must
add some checks to make sure its safe to use these functions while also
allowing for shared buffers between src and dest.

* Added bounds and overlapping checks for `copy_buffer`

* Added overlap checks for `blit_image` and `copy_image`

Created utility file `src/command_buffer/validity/ranges.rs` which hold
functions that help compute if there is overlap in regions.

* Fixed bug in regions overlap check

It should be ANDed together and not ORed

* Added `image_blit` to the `image-self-copy` example

* Added example for `copy_buffer_dimensions` using `basic-compute-shaders`

Also updated copyright year for this and the `image-self-copy-blit`
example.

vulkano/src/command_buffer/synced/commands.rs
@@ -329,17 +329,36 @@ impl SyncCommandBufferBuilder {
             }
         }
 
-        self.append_command(
-            Cmd {
-                source: source.clone(),
-                source_layout,
-                destination: destination.clone(),
-                destination_layout,
-                regions: Mutex::new(Some(regions)),
-            },
-            [
+        let mut resources: SmallVec<[_; 2]> = SmallVec::new();
+
+        // if its the same image in source and destination, we need to lock it once
+        if source.conflict_key() == destination.conflict_key() {
+            resources.push((
+                KeyTy::Image(source.clone()),
+                "source_and_destination".into(),
+                Some((
+                    PipelineMemoryAccess {
+                        stages: PipelineStages {
+                            transfer: true,
+                            ..PipelineStages::none()
+                        },
+                        access: AccessFlags {
+                            transfer_read: true,
+                            transfer_write: true,
+                            ..AccessFlags::none()
+                        },
+                        exclusive: true,
+                    },
+                    // TODO: should, we take the layout as parameter? if so, which? source or destination?
+                    ImageLayout::General,
+                    ImageLayout::General,
+                    ImageUninitializedSafe::Safe,
+                )),
+            ));
+        } else {
+            resources.extend([
                 (
-                    KeyTy::Image(source),
+                    KeyTy::Image(source.clone()),
                     "source".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -359,7 +378,7 @@ impl SyncCommandBufferBuilder {
                     )),
                 ),
                 (
-                    KeyTy::Image(destination),
+                    KeyTy::Image(destination.clone()),
                     "destination".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -378,7 +397,18 @@ impl SyncCommandBufferBuilder {
                         ImageUninitializedSafe::Safe,
                     )),
                 ),
-            ],
+            ]);
+        }
+
+        self.append_command(
+            Cmd {
+                source,
+                source_layout,
+                destination,
+                destination_layout,
+                regions: Mutex::new(Some(regions)),
+            },
+            resources,
         )?;
 
         Ok(())
@@ -430,18 +460,36 @@ impl SyncCommandBufferBuilder {
             }
         }
 
-        self.append_command(
-            Cmd {
-                source: source.clone(),
-                source_layout,
-                destination: destination.clone(),
-                destination_layout,
-                regions: Mutex::new(Some(regions)),
-                filter,
-            },
-            [
+        let mut resources: SmallVec<[_; 2]> = SmallVec::new();
+
+        // if its the same image in source and destination, we need to lock it once
+        if source.conflict_key() == destination.conflict_key() {
+            resources.push((
+                KeyTy::Image(source.clone()),
+                "source_and_destination".into(),
+                Some((
+                    PipelineMemoryAccess {
+                        stages: PipelineStages {
+                            transfer: true,
+                            ..PipelineStages::none()
+                        },
+                        access: AccessFlags {
+                            transfer_read: true,
+                            transfer_write: true,
+                            ..AccessFlags::none()
+                        },
+                        exclusive: true,
+                    },
+                    // TODO: should, we take the layout as parameter? if so, which? source or destination?
+                    ImageLayout::General,
+                    ImageLayout::General,
+                    ImageUninitializedSafe::Safe,
+                )),
+            ));
+        } else {
+            resources.extend([
                 (
-                    KeyTy::Image(source),
+                    KeyTy::Image(source.clone()),
                     "source".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -461,7 +509,7 @@ impl SyncCommandBufferBuilder {
                     )),
                 ),
                 (
-                    KeyTy::Image(destination),
+                    KeyTy::Image(destination.clone()),
                     "destination".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -480,7 +528,19 @@ impl SyncCommandBufferBuilder {
                         ImageUninitializedSafe::Safe,
                     )),
                 ),
-            ],
+            ]);
+        }
+
+        self.append_command(
+            Cmd {
+                source,
+                source_layout,
+                destination,
+                destination_layout,
+                regions: Mutex::new(Some(regions)),
+                filter,
+            },
+            resources,
         )?;
 
         Ok(())
@@ -713,15 +773,35 @@ impl SyncCommandBufferBuilder {
             }
         }
 
-        self.append_command(
-            Cmd {
-                source: source.clone(),
-                destination: destination.clone(),
-                regions: Mutex::new(Some(regions)),
-            },
-            [
+        let mut resources: SmallVec<[_; 2]> = SmallVec::new();
+
+        // if its the same image in source and destination, we need to lock it once
+        if source.conflict_key() == destination.conflict_key() {
+            resources.push((
+                KeyTy::Buffer(source.clone()),
+                "source_and_destination".into(),
+                Some((
+                    PipelineMemoryAccess {
+                        stages: PipelineStages {
+                            transfer: true,
+                            ..PipelineStages::none()
+                        },
+                        access: AccessFlags {
+                            transfer_read: true,
+                            transfer_write: true,
+                            ..AccessFlags::none()
+                        },
+                        exclusive: false,
+                    },
+                    ImageLayout::Undefined,
+                    ImageLayout::Undefined,
+                    ImageUninitializedSafe::Unsafe,
+                )),
+            ));
+        } else {
+            resources.extend([
                 (
-                    KeyTy::Buffer(source),
+                    KeyTy::Buffer(source.clone()),
                     "source".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -741,7 +821,7 @@ impl SyncCommandBufferBuilder {
                     )),
                 ),
                 (
-                    KeyTy::Buffer(destination),
+                    KeyTy::Buffer(destination.clone()),
                     "destination".into(),
                     Some((
                         PipelineMemoryAccess {
@@ -760,7 +840,16 @@ impl SyncCommandBufferBuilder {
                         ImageUninitializedSafe::Unsafe,
                     )),
                 ),
-            ],
+            ]);
+        }
+
+        self.append_command(
+            Cmd {
+                source,
+                destination,
+                regions: Mutex::new(Some(regions)),
+            },
+            resources,
         )?;
 
         Ok(())

examples/src/bin/image-self-copy-blit/main.rs
@@ -1,0 +1,448 @@
+// Copyright (c) 2021 The vulkano developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
+
+use std::io::Cursor;
+use std::sync::Arc;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess};
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferUsage, PrimaryCommandBuffer, SubpassContents,
+};
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
+use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType};
+use vulkano::device::{Device, DeviceExtensions, Features};
+use vulkano::format::{ClearValue, Format};
+use vulkano::image::{view::ImageView, ImageDimensions, ImageUsage, SwapchainImage};
+use vulkano::image::{ImageAccess, StorageImage};
+use vulkano::instance::Instance;
+use vulkano::pipeline::graphics::color_blend::ColorBlendState;
+use vulkano::pipeline::graphics::input_assembly::{InputAssemblyState, PrimitiveTopology};
+use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::{GraphicsPipeline, Pipeline, PipelineBindPoint};
+use vulkano::render_pass::{Framebuffer, RenderPass, Subpass};
+use vulkano::sampler::{Filter, MipmapMode, Sampler, SamplerAddressMode};
+use vulkano::swapchain::{self, AcquireError, Swapchain, SwapchainCreationError};
+use vulkano::sync::{self, FlushError, GpuFuture};
+use vulkano::Version;
+use vulkano_win::VkSurfaceBuild;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+fn main() {
+    // The start of this example is exactly the same as `triangle`. You should read the
+    // `triangle` example if you haven't done so yet.
+
+    let required_extensions = vulkano_win::required_extensions();
+    let instance = Instance::new(None, Version::V1_1, &required_extensions, None).unwrap();
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
+    let (physical_device, queue_family) = PhysicalDevice::enumerate(&instance)
+        .filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+        .filter_map(|p| {
+            p.queue_families()
+                .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
+                .map(|q| (p, q))
+        })
+        .min_by_key(|(p, _)| match p.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+        })
+        .unwrap();
+
+    println!(
+        "Using device: {} (type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type,
+    );
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        &Features::none(),
+        &physical_device
+            .required_extensions()
+            .union(&device_extensions),
+        [(queue_family, 0.5)].iter().cloned(),
+    )
+    .unwrap();
+    let queue = queues.next().unwrap();
+
+    let (mut swapchain, images) = {
+        let caps = surface.capabilities(physical_device).unwrap();
+        let composite_alpha = caps.supported_composite_alpha.iter().next().unwrap();
+        let format = caps.supported_formats[0].0;
+        let dimensions: [u32; 2] = surface.window().inner_size().into();
+
+        Swapchain::start(device.clone(), surface.clone())
+            .num_images(caps.min_image_count)
+            .format(format)
+            .dimensions(dimensions)
+            .usage(ImageUsage::color_attachment())
+            .sharing_mode(&queue)
+            .composite_alpha(composite_alpha)
+            .build()
+            .unwrap()
+    };
+
+    #[repr(C)]
+    #[derive(Default, Debug, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    vulkano::impl_vertex!(Vertex, position);
+
+    let vertex_buffer = CpuAccessibleBuffer::<[Vertex]>::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        false,
+        [
+            Vertex {
+                position: [-0.5, -0.5],
+            },
+            Vertex {
+                position: [-0.5, 0.5],
+            },
+            Vertex {
+                position: [0.5, -0.5],
+            },
+            Vertex {
+                position: [0.5, 0.5],
+            },
+        ]
+        .iter()
+        .cloned(),
+    )
+    .unwrap();
+
+    let vs = vs::load(device.clone()).unwrap();
+    let fs = fs::load(device.clone()).unwrap();
+
+    let render_pass = vulkano::single_pass_renderpass!(device.clone(),
+        attachments: {
+            color: {
+                load: Clear,
+                store: Store,
+                format: swapchain.format(),
+                samples: 1,
+            }
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {}
+        }
+    )
+    .unwrap();
+
+    let (texture, tex_future) = {
+        let png_bytes = include_bytes!("image_img.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let img_size = [info.width, info.height];
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width * 2,
+            height: info.height * 2,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        let image = StorageImage::new(
+            device.clone(),
+            dimensions,
+            Format::R8G8B8A8_UNORM,
+            [queue.family()],
+        )
+        .unwrap();
+
+        let buffer = CpuAccessibleBuffer::from_iter(
+            device.clone(),
+            BufferUsage::transfer_source(),
+            false,
+            image_data.iter().cloned(),
+        )
+        .unwrap();
+
+        let mut builder = AutoCommandBufferBuilder::primary(
+            device.clone(),
+            queue.family(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+        // here, we perform image copying and blitting on the same image
+        builder
+            //  clear the image buffer
+            .clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 0.0, 0.0]))
+            .unwrap()
+            // put our image in the top left corner
+            .copy_buffer_to_image_dimensions(
+                buffer,
+                image.clone(),
+                [0, 0, 0],
+                [img_size[0], img_size[1], 1],
+                0,
+                1,
+                0,
+            )
+            .unwrap()
+            // copy from the top left corner to the bottom right corner
+            .copy_image(
+                image.clone(),
+                [0, 0, 0],
+                0,
+                0,
+                image.clone(),
+                [img_size[0] as i32, img_size[1] as i32, 0],
+                0,
+                0,
+                [img_size[0], img_size[1], 1],
+                1,
+            )
+            .unwrap()
+            // blit from the bottom right corner to the top right corner (flipped)
+            .blit_image(
+                image.clone(),
+                [img_size[0] as i32, img_size[1] as i32, 0],
+                [img_size[0] as i32 * 2, img_size[1] as i32 * 2, 1],
+                0,
+                0,
+                image.clone(),
+                // flipping the top_left and bottom_right corners results in flipped image
+                [img_size[0] as i32 * 2 - 1, img_size[1] as i32 - 1, 0],
+                [img_size[0] as i32, 0, 1],
+                0,
+                0,
+                1,
+                Filter::Nearest,
+            )
+            .unwrap();
+        let command_buffer = builder.build().unwrap();
+
+        (
+            ImageView::new(image).unwrap(),
+            command_buffer.execute(queue.clone()).unwrap(),
+        )
+    };
+
+    let sampler = Sampler::new(
+        device.clone(),
+        Filter::Linear,
+        Filter::Linear,
+        MipmapMode::Nearest,
+        SamplerAddressMode::Repeat,
+        SamplerAddressMode::Repeat,
+        SamplerAddressMode::Repeat,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+    )
+    .unwrap();
+
+    let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
+    let pipeline = GraphicsPipeline::start()
+        .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
+        .vertex_shader(vs.entry_point("main").unwrap(), ())
+        .input_assembly_state(InputAssemblyState::new().topology(PrimitiveTopology::TriangleStrip))
+        .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
+        .fragment_shader(fs.entry_point("main").unwrap(), ())
+        .color_blend_state(ColorBlendState::new(subpass.num_color_attachments()).blend_alpha())
+        .render_pass(subpass)
+        .build(device.clone())
+        .unwrap();
+
+    let layout = pipeline.layout().descriptor_set_layouts().get(0).unwrap();
+    let set = PersistentDescriptorSet::new(
+        layout.clone(),
+        [WriteDescriptorSet::image_view_sampler(
+            0,
+            texture.clone(),
+            sampler.clone(),
+        )],
+    )
+    .unwrap();
+
+    let mut viewport = Viewport {
+        origin: [0.0, 0.0],
+        dimensions: [0.0, 0.0],
+        depth_range: 0.0..1.0,
+    };
+    let mut framebuffers = window_size_dependent_setup(&images, render_pass.clone(), &mut viewport);
+
+    let mut recreate_swapchain = false;
+    let mut previous_frame_end = Some(tex_future.boxed());
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(_),
+            ..
+        } => {
+            recreate_swapchain = true;
+        }
+        Event::RedrawEventsCleared => {
+            previous_frame_end.as_mut().unwrap().cleanup_finished();
+
+            if recreate_swapchain {
+                let dimensions: [u32; 2] = surface.window().inner_size().into();
+                let (new_swapchain, new_images) =
+                    match swapchain.recreate().dimensions(dimensions).build() {
+                        Ok(r) => r,
+                        Err(SwapchainCreationError::UnsupportedDimensions) => return,
+                        Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
+                    };
+
+                swapchain = new_swapchain;
+                framebuffers =
+                    window_size_dependent_setup(&new_images, render_pass.clone(), &mut viewport);
+                recreate_swapchain = false;
+            }
+
+            let (image_num, suboptimal, acquire_future) =
+                match swapchain::acquire_next_image(swapchain.clone(), None) {
+                    Ok(r) => r,
+                    Err(AcquireError::OutOfDate) => {
+                        recreate_swapchain = true;
+                        return;
+                    }
+                    Err(e) => panic!("Failed to acquire next image: {:?}", e),
+                };
+
+            if suboptimal {
+                recreate_swapchain = true;
+            }
+
+            let clear_values = vec![[0.0, 0.0, 1.0, 1.0].into()];
+            let mut builder = AutoCommandBufferBuilder::primary(
+                device.clone(),
+                queue.family(),
+                CommandBufferUsage::OneTimeSubmit,
+            )
+            .unwrap();
+            builder
+                .begin_render_pass(
+                    framebuffers[image_num].clone(),
+                    SubpassContents::Inline,
+                    clear_values,
+                )
+                .unwrap()
+                .set_viewport(0, [viewport.clone()])
+                .bind_pipeline_graphics(pipeline.clone())
+                .bind_descriptor_sets(
+                    PipelineBindPoint::Graphics,
+                    pipeline.layout().clone(),
+                    0,
+                    set.clone(),
+                )
+                .bind_vertex_buffers(0, vertex_buffer.clone())
+                .draw(vertex_buffer.len() as u32, 1, 0, 0)
+                .unwrap()
+                .end_render_pass()
+                .unwrap();
+            let command_buffer = builder.build().unwrap();
+
+            let future = previous_frame_end
+                .take()
+                .unwrap()
+                .join(acquire_future)
+                .then_execute(queue.clone(), command_buffer)
+                .unwrap()
+                .then_swapchain_present(queue.clone(), swapchain.clone(), image_num)
+                .then_signal_fence_and_flush();
+
+            match future {
+                Ok(future) => {
+                    previous_frame_end = Some(future.boxed());
+                }
+                Err(FlushError::OutOfDate) => {
+                    recreate_swapchain = true;
+                    previous_frame_end = Some(sync::now(device.clone()).boxed());
+                }
+                Err(e) => {
+                    println!("Failed to flush future: {:?}", e);
+                    previous_frame_end = Some(sync::now(device.clone()).boxed());
+                }
+            }
+        }
+        _ => (),
+    });
+}
+
+/// This method is called once during initialization, then again whenever the window is resized
+fn window_size_dependent_setup(
+    images: &[Arc<SwapchainImage<Window>>],
+    render_pass: Arc<RenderPass>,
+    viewport: &mut Viewport,
+) -> Vec<Arc<Framebuffer>> {
+    let dimensions = images[0].dimensions().width_height();
+    viewport.dimensions = [dimensions[0] as f32, dimensions[1] as f32];
+
+    images
+        .iter()
+        .map(|image| {
+            let view = ImageView::new(image.clone()).unwrap();
+            Framebuffer::start(render_pass.clone())
+                .add(view)
+                .unwrap()
+                .build()
+                .unwrap()
+        })
+        .collect::<Vec<_>>()
+}
+
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: "
+#version 450
+
+layout(location = 0) in vec2 position;
+layout(location = 0) out vec2 tex_coords;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    tex_coords = position + vec2(0.5);
+}"
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: "
+#version 450
+
+layout(location = 0) in vec2 tex_coords;
+layout(location = 0) out vec4 f_color;
+
+layout(set = 0, binding = 0) uniform sampler2D tex;
+
+void main() {
+    f_color = texture(tex, tex_coords);
+}"
+    }
+}

commit 5d04cdc17de37e0d03e8e2754090c1957af80f06

Fix sync bug in same image blit/copy commands
The bug was introduced by #1782.
When we implemented "same image blit/copy" feature,
we added only `one` image to the reserved resources for the command if
the same image was used as `source` and `destination`.

The bug happens if we passed the same image, but with different
mip level and/or array layer. In that case, we will only add the
`source` resource and not the `destination`, and that would result in
failing to know when to flush the pipeline barrier.

vulkano/src/command_buffer/synced/commands.rs
@@ -332,7 +332,9 @@ impl SyncCommandBufferBuilder {
         let mut resources: SmallVec<[_; 2]> = SmallVec::new();
 
         // if its the same image in source and destination, we need to lock it once
-        if source.conflict_key() == destination.conflict_key() {
+        let source_key = ResourceKey::from(source.as_ref());
+        let destination_key = ResourceKey::from(destination.as_ref());
+        if source_key == destination_key {
             resources.push((
                 KeyTy::Image(source.clone()),
                 "source_and_destination".into(),
@@ -463,7 +465,9 @@ impl SyncCommandBufferBuilder {
         let mut resources: SmallVec<[_; 2]> = SmallVec::new();
 
         // if its the same image in source and destination, we need to lock it once
-        if source.conflict_key() == destination.conflict_key() {
+        let source_key = ResourceKey::from(source.as_ref());
+        let destination_key = ResourceKey::from(destination.as_ref());
+        if source_key == destination_key {
             resources.push((
                 KeyTy::Image(source.clone()),
                 "source_and_destination".into(),