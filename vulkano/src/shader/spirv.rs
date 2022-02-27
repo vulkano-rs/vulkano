5aa51c9069745e9bb9ccee79f01ce386aa69a9bb
White-Green <43771790+White-Green@users.noreply.github.com>
2022-02-28 00:58:57 +0900

    allow OpUndef in function body

--- vulkano/src/shader/spirv.rs
+++ vulkano/src/shader/spirv.rs
@@ -229,8 +229,8 @@ impl Spirv {
                 | Instruction::SpecConstantFalse { .. }
                 | Instruction::SpecConstant { .. }
                 | Instruction::SpecConstantComposite { .. }
-                | Instruction::SpecConstantOp { .. }
-                | Instruction::Undef { .. } => set_range(&mut range_global, index)?,
+                | Instruction::SpecConstantOp { .. } => set_range(&mut range_global, index)?,
+                Instruction::Undef { .. } if !in_function => set_range(&mut range_global, index)?,
                 Instruction::Variable { storage_class, .. }
                     if *storage_class != StorageClass::Function =>
                 {
