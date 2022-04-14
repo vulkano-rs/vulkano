5cd20e059ad9f134fc085ee6dbca9a7d7cc9ac55
Zhixing Zhang <me@neoto.xin>
2022-04-14 01:13:15 -0700

    add comments & remove build script

--- vulkano/build.rs
+++ vulkano/build.rs
@@ -6,24 +6,9 @@
 // at your option. All files in the project carrying such
 // notice may not be copied, modified, or distributed except
 // according to those terms.
-
-use std::env;
-
 mod autogen;
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    if target.contains("apple-ios") {
-        println!("cargo:rustc-link-search=framework=/Library/Frameworks/");
-        println!("cargo:rustc-link-lib=c++");
-        println!("cargo:rustc-link-lib=framework=MoltenVK");
-        println!("cargo:rustc-link-lib=framework=Metal");
-        println!("cargo:rustc-link-lib=framework=IOSurface");
-        println!("cargo:rustc-link-lib=framework=QuartzCore");
-        println!("cargo:rustc-link-lib=framework=UIKit");
-        println!("cargo:rustc-link-lib=framework=Foundation");
-    }
-
     // Run autogen
     println!("cargo:rerun-if-changed=vk.xml");
     autogen::autogen();
--- vulkano/src/instance/library.rs
+++ vulkano/src/instance/library.rs
@@ -10,17 +10,20 @@
 #[cfg(feature = "loaded")]
 pub use ash::LoadingError;
 
+/// Wraps the `ash::Entry` type which contains function pointers into the Vulkan library.
 pub struct VulkanLibrary {
     entry: ash::Entry,
 }
 
 impl VulkanLibrary {
+    /// Load entry points from a Vulkan loader linked at compile time.
     #[cfg(feature = "linked")]
     pub fn linked() -> VulkanLibrary {
         let entry = ash::Entry::linked();
         VulkanLibrary { entry }
     }
 
+    /// Load default Vulkan library for the current platform with `libloading`.
     #[cfg(feature = "loaded")]
     pub unsafe fn load() -> Result<VulkanLibrary, LoadingError> {
         let entry = ash::Entry::load()?;
@@ -33,6 +36,8 @@ impl VulkanLibrary {
 }
 
 impl Default for VulkanLibrary {
+    /// Returns the default vulkan library. The way that the Vulkan library was
+    /// loaded depends on the feature flags.
     fn default() -> Self {
         #[cfg(feature = "linked")]
         let entry = VulkanLibrary::linked();
