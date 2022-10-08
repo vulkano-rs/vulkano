2ccd979e6f1e36807fa59dd889b2ae41b77f9da0
marc0246 <40955683+marc0246@users.noreply.github.com>
2022-10-08 13:46:30 +0200

    Oopsie

--- vulkano/src/buffer/traits.rs
+++ vulkano/src/buffer/traits.rs
@@ -8,7 +8,7 @@
 // according to those terms.
 
 use super::{sys::UnsafeBuffer, BufferContents, BufferSlice, BufferUsage};
-use crate::{device::DeviceOwned, DeviceSize, RequiresOneOf, SafeDeref, VulkanObject};
+use crate::{device::DeviceOwned, DeviceSize, RequiresOneOf, SafeDeref, Version, VulkanObject};
 use std::{
     error::Error,
     fmt::{Debug, Display, Error as FmtError, Formatter},
@@ -100,10 +100,12 @@ pub unsafe trait BufferAccess: DeviceOwned + Send + Sync {
                 ..Default::default()
             };
             let fns = device.fns();
-            let f = if device.enabled_extensions().ext_buffer_device_address {
-                fns.ext_buffer_device_address.get_buffer_device_address_ext
-            } else {
+            let f = if device.api_version() >= Version::V1_2 {
+                fns.v1_2.get_buffer_device_address
+            } else if device.enabled_extensions().khr_buffer_device_address {
                 fns.khr_buffer_device_address.get_buffer_device_address_khr
+            } else {
+                fns.ext_buffer_device_address.get_buffer_device_address_ext
             };
             let ptr = f(device.internal_object(), &info);
 
