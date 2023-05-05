e3f99a2f506f058892e08ecc9d06650ec1db213b
Rua <ruawhitepaw@gmail.com>
2023-05-05 11:59:34 +0200

    Update vulkano/src/lib.rs
    Co-authored-by: marc0246 <40955683+marc0246@users.noreply.github.com>

--- vulkano/src/lib.rs
+++ vulkano/src/lib.rs
@@ -275,7 +275,6 @@ impl From<ValidationError> for VulkanError {
 }
 
 impl From<RuntimeError> for VulkanError {
-    #[inline]
     fn from(err: RuntimeError) -> Self {
         Self::RuntimeError(err)
     }
