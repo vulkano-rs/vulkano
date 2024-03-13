f1e3aeee604814a98c63e444fd7e42ed9e410461
maratik123 <marat.buharov@gmail.com>
2024-03-13 12:39:37 +0300

    Update vulkano/src/command_buffer/commands/query.rs
    Co-authored-by: marc0246 <40955683+marc0246@users.noreply.github.com>

--- vulkano/src/command_buffer/commands/query.rs
+++ vulkano/src/command_buffer/commands/query.rs
@@ -252,7 +252,6 @@ impl RecordingCommandBuffer {
     /// See also [`get_results`].
     ///
     /// [`query_pool.ty().result_len()`]: QueryPool::result_len
-    /// [`QueryResultFlags::WITH_AVAILABILITY`]: QueryResultFlags::WITH_AVAILABILITY
     /// [`get_results`]: QueryPool::get_results
     pub fn copy_query_pool_results<T>(
         &mut self,
