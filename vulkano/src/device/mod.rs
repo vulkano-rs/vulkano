9457f4c0b100ed7dc5498246ab17d6142cbe015a
Matusson <41548922+Matusson@users.noreply.github.com>
2026-01-07 10:13:54 +0100

    Validate `ext_debug_utils` extension when calling `set_debug_utils_object_name` (#2762)
    * Validate debug utils extension
    
    * Address PR comments

--- vulkano/src/device/mod.rs
+++ vulkano/src/device/mod.rs
@@ -1299,17 +1299,50 @@ impl Device {
 
     /// Assigns a human-readable name to `object` for debugging purposes.
     ///
+    /// The [`ext_debug_utils`](crate::instance::InstanceExtensions::ext_debug_utils) extension
+    /// must be enabled on the instance.
+    ///
     /// If `object_name` is `None`, a previously set object name is removed.
     ///
-    /// # Panics
-    /// - If `object` is not owned by this device.
-    pub fn set_debug_utils_object_name<T: VulkanObject + DeviceOwned>(
+    /// # Safety
+    ///
+    /// - `object` must not be used concurrently by any other Vulkan command.
+    #[inline]
+    pub unsafe fn set_debug_utils_object_name<T: VulkanObject + DeviceOwned>(
+        &self,
+        object: &T,
+        object_name: Option<&str>,
+    ) -> Result<(), Validated<VulkanError>> {
+        self.validate_set_debug_utils_object_name(object)?;
+
+        Ok(unsafe { self.set_debug_utils_object_name_unchecked(object, object_name) }?)
+    }
+
+    fn validate_set_debug_utils_object_name<T: VulkanObject + DeviceOwned>(
+        &self,
+        object: &T,
+    ) -> Result<(), Box<ValidationError>> {
+        // VUID-vkSetDebugUtilsObjectNameEXT-pNameInfo-07874
+        assert_eq!(object.device().handle(), self.handle());
+
+        if !self.instance().enabled_extensions().ext_debug_utils {
+            return Err(Box::new(ValidationError {
+                requires_one_of: RequiresOneOf(&[RequiresAllOf(&[Requires::InstanceExtension(
+                    "ext_debug_utils",
+                )])]),
+                ..Default::default()
+            }));
+        }
+
+        Ok(())
+    }
+
+    #[cfg_attr(not(feature = "document_unchecked"), doc(hidden))]
+    pub unsafe fn set_debug_utils_object_name_unchecked<T: VulkanObject + DeviceOwned>(
         &self,
         object: &T,
         object_name: Option<&str>,
     ) -> Result<(), VulkanError> {
-        assert_eq!(object.device().handle(), self.handle());
-
         let object_name_vk = object_name.map(|object_name| CString::new(object_name).unwrap());
         let mut info_vk = vk::DebugUtilsObjectNameInfoEXT::default().object_handle(object.handle());
 
@@ -2135,16 +2168,29 @@ where
 pub unsafe trait DeviceOwnedVulkanObject {
     /// Assigns a human-readable name to the object for debugging purposes.
     ///
+    /// The [`ext_debug_utils`](crate::instance::InstanceExtensions::ext_debug_utils) extension
+    /// must be enabled on the instance.
+    ///
     /// If `object_name` is `None`, a previously set object name is removed.
-    fn set_debug_utils_object_name(&self, object_name: Option<&str>) -> Result<(), VulkanError>;
+    ///
+    /// # Safety
+    ///
+    /// - The object must not be used concurrently by any other Vulkan command.
+    unsafe fn set_debug_utils_object_name(
+        &self,
+        object_name: Option<&str>,
+    ) -> Result<(), Validated<VulkanError>>;
 }
 
 unsafe impl<T> DeviceOwnedVulkanObject for T
 where
     T: DeviceOwned + VulkanObject,
 {
-    fn set_debug_utils_object_name(&self, object_name: Option<&str>) -> Result<(), VulkanError> {
-        self.device().set_debug_utils_object_name(self, object_name)
+    unsafe fn set_debug_utils_object_name(
+        &self,
+        object_name: Option<&str>,
+    ) -> Result<(), Validated<VulkanError>> {
+        unsafe { self.device().set_debug_utils_object_name(self, object_name) }
     }
 }
 
