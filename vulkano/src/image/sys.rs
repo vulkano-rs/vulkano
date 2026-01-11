6d6990e5038b1c349100b04bab4e2b3d6b28423e
galister <22305755+galister@users.noreply.github.com>
2026-01-11 03:04:44 +0000

    image creation with drm modifier but no planes VUID 02261 (#2764)

--- vulkano/src/image/sys.rs
+++ vulkano/src/image/sys.rs
@@ -3060,8 +3060,8 @@ impl<'a> ImageCreateInfo<'a> {
                 .plane_layouts(plane_layouts_vk)
         });
 
-        let drm_format_modifier_list_vk = (!self.drm_format_modifier_plane_layouts.is_empty())
-            .then(|| {
+        let drm_format_modifier_list_vk =
+            (plane_layouts_vk.is_empty() && !self.drm_format_modifiers.is_empty()).then(|| {
                 vk::ImageDrmFormatModifierListCreateInfoEXT::default()
                     .drm_format_modifiers(self.drm_format_modifiers)
             });
